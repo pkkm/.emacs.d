0849ff3135dc202d1528fa4282bd925e7781d86c
Paweł Kraśnicki <dev@pkkm.eu>
2014-07-21 17:32:23 +0200

    Disable automatic committing of changes

--- update-github.sh
+++ update-github.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env sh
 
 # Update the GitHub .emacs.d repository.
-
-git add --all init.el conf vendor snippets update-github.sh
-git add -u
-
-git commit -m "Update"
-
 git push origin master
