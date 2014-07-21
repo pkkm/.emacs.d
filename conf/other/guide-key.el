95f3cb76f467d60d2cd32ded9573d29697c2c623
Paweł Kraśnicki <dev@pkkm.eu>
2014-07-21 17:55:42 +0200

    Enable guide-key for more prefixes, with a shorter delay

--- conf/other/guide-key.el
+++ conf/other/guide-key.el
@@ -4,8 +4,11 @@
 (package-ensure-installed 'guide-key)
 (guide-key-mode 1)
 
+(setq guide-key/idle-delay 0.5)
+
+(setq guide-key/recursive-key-sequence-flag t) ; If `guide-key' is enabled for a keymap, also enable it for its subkeymaps.
 (setq guide-key/guide-key-sequence
-      '("e RET" "e C-n"))
+      '("e" "C-x" "SPC" "C-c" "e RET" "C-x RET" "e C-n" "C-x n"))
 
 ;; Don't show the minor mode in the modeline.
 (require 'conf/modeline/cleaner-minor-modes)
