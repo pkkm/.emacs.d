0f52af7b550353de721d3cf7021a5274aa581af8
Paweł Kraśnicki <dev@pkkm.eu>
2014-07-31 17:49:31 +0200

    Add settings for TRAMP multi-hop connections

--- conf/opening-saving/tramp.el
+++ conf/opening-saving/tramp.el
@@ -1,6 +1,17 @@
 ;;; TRAMP -- transparent manipulating of remote files.
 
+;; Perform multiple hops like this:
+;;   /ssh:root@server|su:user@localhost:/path/to/file
+(setq tramp-save-ad-hoc-proxies nil) ; Don't permanently save such multi-hops.
+
+;; Automatic multi-hop proxies.
+(setq tramp-default-proxies-alist
+      ;; When attempting to open a file on llaesh as a non-root user (e.g. "/su:user@llaesh:/file"), tunnel through "ssh root@llaesh".
+      `((,(rx string-start "llaesh" string-end) (rx string-start "root" string-end) nil) ; Don't tunnel for root (to avoid infinite recursion).
+        (,(rx string-start "llaesh" string-end) nil "/ssh:root@llaesh:"))) ; Tunnel for everyone else.
+
 (setq tramp-persistency-file-name
       (expand-file-name "tramp" my-savefile-dir))
+
 
 (provide 'conf/opening-saving/tramp)
