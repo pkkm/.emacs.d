6b8ebaaf713834a4b3b61a654d83dfee26d64b51
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-15 19:21:10 +0200

    Don't show magit-auto-revert-mode in the modeline

--- conf/opening-saving/version-control.el
+++ conf/opening-saving/version-control.el
@@ -1,10 +1,23 @@
 ;;; Version control interfaces.
 
-;; Use Magit.
+
+;;; Magit (git interface).
+
 (require 'conf/packages)
 (package-ensure-installed 'magit)
 
-;; Disable VC-mode (the default Emacs interface for VCSe
+;; Don't show magit-auto-revert-mode in the modeline.
+;; (This minor mode reverts unmodified buffers affected by git commands.)
+(require 'conf/utils/hooks)
+(add-one-shot-hooks '(magit-mode-hook magit-auto-revert-mode-hook)
+                    #'diminish-magit-auto-revert-mode)
+(defun diminish-magit-auto-revert-mode ()
+  (require 'conf/modeline/cleaner-minor-modes)
+  (diminish 'magit-auto-revert-mode))
+
+
+;;; Disable VC-mode (the default Emacs interface for VCSes).
 (setq vc-handled-backends '())
+
 
 (provide 'conf/opening-saving/version-control)
