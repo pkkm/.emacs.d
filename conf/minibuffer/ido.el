270b0494015934794710acd931caa0cb3a183fb1
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-21 23:58:00 +0200

    Convert conf/minibuffer/ido.el to use-package

--- conf/minibuffer/ido.el
+++ conf/minibuffer/ido.el
@@ -3,44 +3,53 @@
 
 (require 'conf/packages)
 
-(ido-mode 1) ; Bundled package.
+(use-package ido ; Bundled with Emacs.
+  :demand t
+  :config
 
-;; Use Ido almost everywhere.
-(package-ensure-installed 'ido-ubiquitous)
-(ido-ubiquitous-mode 1)
+  (ido-mode 1)
 
-;; Flex matching. On low-end machines, use the builtin algorithm; elsewhere, use a better, memory-hungry one.
-(setq ido-enable-flex-matching t)
-(package-ensure-installed 'flx-ido)
-(unless (eq (getenv "LOW_END_MACHINE") "true")
-  (flx-ido-mode 1))
+  ;; Use Ido almost everywhere.
+  (ido-ubiquitous-mode)
 
-;; Savefile.
-(setq ido-save-directory-list-file
-      (expand-file-name "ido" my-savefile-dir))
+  ;; Flex matching.
+  (setq ido-enable-flex-matching t)
+  ;; On high-end machines, use a better, more memory-hungry algorithm.
+  (unless (eq (getenv "LOW_END_MACHINE") "true")
+    (flx-ido-mode 1))
 
-;; Display completions vertically.
-(setq ido-decorations '("\n- "
-                        ""
-                        "\n  "
-                        "\n  ..."
-                        "["
-                        "]"
-                        " [No match]"
-                        " [Matched]"
-                        " [Not readable]"
-                        " [Too big]"
-                        " [Confirm]"))
-(defun disable-line-truncation ()
-  "Disable line truncation in the current buffer.
+  ;; Savefile.
+  (setq ido-save-directory-list-file
+        (expand-file-name "ido" my-savefile-dir))
+
+  ;; Display completions vertically.
+  (setq ido-decorations '("\n- "
+                          ""
+                          "\n  "
+                          "\n  ..."
+                          "["
+                          "]"
+                          " [No match]"
+                          " [Matched]"
+                          " [Not readable]"
+                          " [Too big]"
+                          " [Confirm]"))
+  (defun disable-line-truncation ()
+    "Disable line truncation in the current buffer.
 This is needed for the vertical displaying of Ido completions to work."
-  (set (make-local-variable 'truncate-lines) nil))
-(add-hook 'ido-minibuffer-setup-hook #'disable-line-truncation)
+    (set (make-local-variable 'truncate-lines) nil))
+  (add-hook 'ido-minibuffer-setup-hook #'disable-line-truncation)
 
-;; Keybindings.
-(defun ido-define-keys ()
-  (define-key ido-completion-map (kbd "C-n") #'ido-next-match)
-  (define-key ido-completion-map (kbd "C-p") #'ido-prev-match))
-(add-hook 'ido-setup-hook #'ido-define-keys)
+  ;; Keybindings.
+  (bind-key "C-n" #'ido-next-match ido-completion-map)
+  (bind-key "C-p" #'ido-prev-match ido-completion-map))
+
+(use-package ido-ubiquitous
+  :ensure ido-ubiquitous
+  :commands ido-ubiquitous-mode)
+
+(use-package flx-ido
+  :ensure flx-ido
+  :commands flx-ido-mode)
 
 (provide 'conf/minibuffer/ido)
