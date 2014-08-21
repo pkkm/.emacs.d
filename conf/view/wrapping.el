8f12de141fda337b29ecf66576484bec47ff0171
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-21 23:33:06 +0200

    Convert conf/view/wrapping.el to use-package

--- conf/view/wrapping.el
+++ conf/view/wrapping.el
@@ -4,10 +4,12 @@
 (setq-default word-wrap t)
 
 ;; Indent wrapped parts the same as original lines.
-;; The code is in vendor/adaptive-wrap.el -- this is the version from <https://gist.github.com/tsavola/6222431>, which handles tabs properly, in contrast to the version on ELPA (as of Dec 2013).
-(autoload 'adaptive-wrap-prefix-mode "adaptive-wrap")
-(define-globalized-minor-mode global-adaptive-wrap-prefix-mode
-  adaptive-wrap-prefix-mode adaptive-wrap-prefix-mode)
-(global-adaptive-wrap-prefix-mode 1)
+(use-package adaptive-wrap ; Installed in vendor/adaptive-wrap.el -- this is the version from <https://gist.github.com/tsavola/6222431>, which handles tabs properly, in contrast to the version on ELPA (as of Dec 2013).
+  :demand t
+  :commands adaptive-wrap-prefix-mode
+  :config
+  (define-globalized-minor-mode global-adaptive-wrap-prefix-mode
+    adaptive-wrap-prefix-mode adaptive-wrap-prefix-mode)
+  (global-adaptive-wrap-prefix-mode))
 
 (provide 'conf/view/wrapping)
