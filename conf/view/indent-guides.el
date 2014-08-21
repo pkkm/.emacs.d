dca267187bccb0c7f450aca5cfb013499fd74a6e
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-21 23:37:37 +0200

    Convert conf/view/indent-guides.el to use-package

--- conf/view/indent-guides.el
+++ conf/view/indent-guides.el
@@ -1,27 +1,23 @@
 ;;; Indentation guides.
 
 (require 'conf/packages)
-(package-ensure-installed 'indent-guide)
+(require 'conf/utils/colors) ; Used: color-mix.
 
-;; We need to load now instead of autoloading, because otherwise `diminish' won't work.
-(require 'indent-guide)
-;;(autoload 'indent-guide-global-mode "indent-guide")
-;;(autoload 'indent-guide-mode "indent-guide")
+(use-package indent-guide
+  :ensure indent-guide
+  :demand t
+  :diminish indent-guide-mode
+  :config
 
-;; Don't display this minor mode in the modeline.
-(require 'conf/modeline/cleaner-minor-modes)
-(diminish 'indent-guide-mode)
+  ;; Use a color close to the background.
+  (face-spec-set 'indent-guide-face `((t)) 'face-defface-spec)
+  (defun update-indent-guide-face ()
+    (set-face-foreground 'indent-guide-face
+                         (color-mix (face-foreground 'shadow nil t) 0.4
+                                    (face-background 'default nil t) 0.6)))
+  (add-hook 'after-load-theme-hook #'update-indent-guide-face)
+  (update-indent-guide-face)
 
-;; Use a color close to the background.
-(require 'conf/utils/colors)
-(face-spec-set 'indent-guide-face `((t)) 'face-defface-spec)
-(defun update-indent-guide-face ()
-  (set-face-foreground 'indent-guide-face
-                       (color-mix (face-foreground 'shadow nil t) 0.4
-                                  (face-background 'default nil t) 0.6)))
-(add-hook 'after-load-theme-hook #'update-indent-guide-face)
-(update-indent-guide-face)
-
-(setq indent-guide-char "│") ; Useful characters: ·│┆┊╎
+  (setq indent-guide-char "│")) ; Useful characters: ·│┆┊╎
 
 (provide 'conf/view/indent-guides)
