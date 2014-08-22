8985f2290883114104f6ba2040bc85b5d276066f
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-22 17:47:03 +0200

    Convert conf/other/games.el to use-package

--- conf/other/games.el
+++ conf/other/games.el
@@ -1,7 +1,11 @@
 ;;; Settings for games.
 
-;; Save game scores in `my-savefile-dir'/games.
-(setq gamegrid-user-score-file-directory
-      (expand-file-name "games" my-savefile-dir))
+(use-package gamegrid ; Bundled with Emacs.
+  :defer t
+  :config
+
+  ;; Save game scores in `my-savefile-dir'/games.
+  (setq gamegrid-user-score-file-directory
+        (expand-file-name "games" my-savefile-dir)))
 
 (provide 'conf/other/games)
