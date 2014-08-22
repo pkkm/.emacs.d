83a90e2635ccbb70ae45343e7ff9cb2db4b8839e
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-22 15:42:12 +0200

    Convert conf/mode-specific/org.el to use-package

--- conf/mode-specific/org.el
+++ conf/mode-specific/org.el
@@ -1,15 +1,16 @@
 ;;; Org-mode.
 
 (require 'conf/packages)
+(require 'conf/evil)
 
-(package-ensure-installed 'org)
+(use-package org
+  :ensure org
+  :defer t
+  :config
 
-(require 'conf/utils/hooks)
-(add-one-shot-hook 'org-mode-hook #'my-org-mode-bindings)
 
-(defun my-org-mode-bindings ()
-  "Add some bindings to Org-mode's keymap.
-This function needs to be run only once in an Emacs session."
+  ;;; Keybindings.
+
   ;; Move/promote/demote headline.
   (define-key org-mode-map (kbd "M-h") #'org-metaleft) ; Shadows org-mark-element.
   (define-key org-mode-map (kbd "M-t") #'org-metadown)
@@ -53,39 +54,42 @@ This function needs to be run only once in an Emacs session."
   ;; Normalize keymaps.
   ;; This is necessary for bindings defined using `evil-define-key' to be active before the first Evil state change.
   ;; See <https://bitbucket.org/lyro/evil/issue/301/evil-define-key-for-minor-mode-does-not>.
-  (evil-normalize-keymaps))
+  (evil-normalize-keymaps)
 
-(defun evil-org-insert-heading ()
-  "Insert a heading in Org-Mode and switch to Evil's insert state."
-  (interactive)
-  (org-end-of-line)
-  (call-interactively #'org-insert-heading)
-  (unless (evil-insert-state-p)
-    (evil-append 1)))
+  (defun evil-org-insert-heading ()
+    "Insert a heading in Org-Mode and switch to Evil's insert state."
+    (interactive)
+    (org-end-of-line)
+    (call-interactively #'org-insert-heading)
+    (unless (evil-insert-state-p)
+      (evil-append 1)))
 
-(defun evil-org-insert-todo-heading ()
-  "Insert a TODO heading in Org-Mode and switch to Evil's insert state."
-  (interactive)
-  (org-end-of-line)
-  (call-interactively #'org-insert-todo-heading)
-  (unless (evil-insert-state-p)
-    (evil-append 1)))
+  (defun evil-org-insert-todo-heading ()
+    "Insert a TODO heading in Org-Mode and switch to Evil's insert state."
+    (interactive)
+    (org-end-of-line)
+    (call-interactively #'org-insert-todo-heading)
+    (unless (evil-insert-state-p)
+      (evil-append 1)))
 
-;; Display.
-(setq org-startup-truncated nil) ; Wrap long lines instead of truncating them (toggle with `toggle-truncate-lines').
-(setq org-src-fontify-natively t) ; Fontify code blocks.
-(setq org-hide-leading-stars t) ; Make leading stars of headlines background color.
-(setq org-fontify-done-headline t) ; Mark the whole headline of a DONE task with a different face.
 
-;; Agenda.
-(setq org-agenda-files
-      (list (when (file-exists-p "~/Org") "~/Org")))
-(global-set-key (kbd "C-x C-a") #'org-agenda)
+  ;;; Other.
 
-;; Logging.
-(setq org-log-repeat nil) ; Don't log shifting forward the date of a repeating task.
+  ;; Display.
+  (setq org-startup-truncated nil) ; Wrap long lines instead of truncating them (toggle with `toggle-truncate-lines').
+  (setq org-src-fontify-natively t) ; Fontify code blocks.
+  (setq org-hide-leading-stars t) ; Make leading stars of headlines background color.
+  (setq org-fontify-done-headline t) ; Mark the whole headline of a DONE task with a different face.
 
-;; Completion.
-(setq org-completion-use-ido t)
+  ;; Agenda.
+  (setq org-agenda-files
+        (list (when (file-exists-p "~/Org") "~/Org")))
+  (global-set-key (kbd "C-x C-a") #'org-agenda)
+
+  ;; Logging.
+  (setq org-log-repeat nil) ; Don't log shifting forward the date of a repeating task.
+
+  ;; Completion.
+  (setq org-completion-use-ido t))
 
 (provide 'conf/mode-specific/org)
