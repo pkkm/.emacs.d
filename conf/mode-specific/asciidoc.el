6f4295f6e15d48f1ddf8e1f376183307001b00c8
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-22 17:14:17 +0200

    Convert conf/mode-specific/asciidoc.el to use-package

--- conf/mode-specific/asciidoc.el
+++ conf/mode-specific/asciidoc.el
@@ -1,21 +1,15 @@
 ;;; AsciiDoc.
 
 (require 'conf/packages)
-(package-ensure-installed 'adoc-mode)
 
-;; Use for .asciidoc files.
-(add-to-list 'auto-mode-alist (cons "\\.asciidoc\\'" 'adoc-mode))
-
-;; Use a variable-pitch face.
-(add-hook 'adoc-mode-hook (lambda ()
-                            (variable-pitch-mode 1)))
-
-;; Disable changing the definition of a paragraph.
-(add-hook 'adoc-mode-hook (lambda ()
-                            (kill-local-variable 'paragraph-separate)
-                            (kill-local-variable 'paragraph-start)
-                            (kill-local-variable 'paragraph-ignore-fill-prefix)))
-
-;; For further customization, see M-x customize-group RET adoc.
+(use-package adoc-mode
+  :ensure adoc-mode
+  :defer t
+  :mode ("\\.asciidoc\\'" . adoc-mode)
+  :config
+  ;; Use a variable-pitch face.
+  (defun turn-on-variable-pitch-mode ()
+    (variable-pitch-mode 1))
+  (add-hook 'adoc-mode-hook #'turn-on-variable-pitch-mode))
 
 (provide 'conf/mode-specific/asciidoc)
