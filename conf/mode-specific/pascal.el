bc0262a670054a16dd002fdbc31bd3d958869272
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-22 17:08:32 +0200

    Convert conf/mode-specific/pascal.el to use-package

--- conf/mode-specific/pascal.el
+++ conf/mode-specific/pascal.el
@@ -1,36 +1,34 @@
 ;;; Pascal.
 
-(require 'conf/utils/hooks) ; Used: add-one-shot-hook.
-
-;; Indentation (Smart Tabs).
+(require 'conf/packages)
 (require 'conf/editing/indentation)
-(smart-tabs-add-language-support pascal pascal-mode-hook
-  ((pascal-indent-line . pascal-indent-level)))
-(defvaralias 'pascal-case-indent 'pascal-indent-level) ; Indent case statements same as everything else.
-(smart-tabs-insinuate 'pascal)
-(add-hook 'pascal-mode-hook #'enable-indent-tabs-mode)
 
-(add-one-shot-hook
- 'pascal-mode-hook
- (lambda ()
-   ;; Don't automatically line up characters.
-   (setq pascal-auto-lineup '())
+(use-package pascal ; Bundled with Emacs.
+  :defer t
+  :config
 
-   ;; Electric characters:
-   ;; # -- unindent if this is a CPP directive.
-   ;; = -- align if containing statement is in `pascal-auto-lineup'.
-   ;; : -- align if containing statement is in `pascal-auto-lineup'.
-   ;; ; -- autoindent.
-   ;; . -- autoindent.
+  ;; Indentation (Smart Tabs).
+  (smart-tabs-add-language-support pascal pascal-mode-hook
+    ((pascal-indent-line . pascal-indent-level)))
+  (defvaralias 'pascal-case-indent 'pascal-indent-level) ; Indent case statements same as everything else.
+  (smart-tabs-insinuate 'pascal)
+  (add-hook 'pascal-mode-hook #'enable-indent-tabs-mode)
 
-   ;; C-c:
-   ;;   C-o -- outline mode.
-   ;;   C-d -- goto defun.
-   ;;   C-c -- comment area.
-   ;;   C-u -- uncomment area.
-   ;;   C-b -- insert block.
+  ;; Electric characters:
+  ;; # -- unindent if this is a CPP directive.
+  ;; ; -- autoindent.
+  ;; . -- autoindent.
+  (setq pascal-auto-lineup '()) ; Don't automatically align characters.
+  ;; = -- align if containing statement is in `pascal-auto-lineup'.
+  ;; : -- align if containing statement is in `pascal-auto-lineup'.
 
-   ;; Disable DEL -- delete and convert the deleted tabs to spaces.
-   (define-key pascal-mode-map (kbd "DEL") nil)))
+  ;; C-c:
+  ;;   C-o -- outline mode.
+  ;;   C-d -- goto defun.
+  ;;   C-b -- insert block.
+  ;;   C-c, C-u -- comment/uncomment area.
+
+  ;; Don't convert tabs to spaces when pressing backspace.
+  (define-key pascal-mode-map (kbd "DEL") nil))
 
 (provide 'conf/mode-specific/pascal)
