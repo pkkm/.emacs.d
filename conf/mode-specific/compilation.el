4876813382bc499cdf033ed749ddbaf35af7bb17
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-22 17:29:16 +0200

    Convert conf/mode-specific/compilation.el to use-package

--- conf/mode-specific/compilation.el
+++ conf/mode-specific/compilation.el
@@ -1,12 +1,13 @@
 ;;; Compilation-mode -- for displaying results of compilation, grep, search, etc.
 
+(require 'conf/packages)
 (require 'conf/evil)
-(require 'conf/utils/keys) ; Used: evil-define-key-in-states.
-(require 'conf/utils/hooks) ; Used: add-one-shot-hook.
 
-(defun my-compilation-mode-bindings ()
+(use-package compile ; Bundled with Emacs.
+  :defer t
+  :config
   (evil-define-key 'motion compilation-mode-map "r" #'recompile)
-  (evil-define-key 'normal compilation-mode-map "r" #'recompile)) ; Necessary to shadow the usual binding.
-(add-one-shot-hook 'compilation-mode-hook #'my-compilation-mode-bindings)
+  (evil-define-key 'normal compilation-mode-map "r" #'recompile) ; Necessary to shadow the usual binding.
+  (evil-normalize-keymaps)) ; So that the bindings take effect immediately.
 
 (provide 'conf/mode-specific/compilation)
