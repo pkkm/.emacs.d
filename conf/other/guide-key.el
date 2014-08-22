6b004b48c6833463e394d82d044a8d7f0d7d9b99
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-22 17:57:32 +0200

    Convert conf/other/guide-key.el to use-package

--- conf/other/guide-key.el
+++ conf/other/guide-key.el
@@ -1,17 +1,20 @@
 ;;; Popups with possible keys after a prefix key.
 
 (require 'conf/packages)
-(package-ensure-installed 'guide-key)
-(guide-key-mode 1)
 
-(setq guide-key/idle-delay 0.5)
-
-(setq guide-key/recursive-key-sequence-flag t) ; If `guide-key' is enabled for a keymap, also enable it for its subkeymaps.
-(setq guide-key/guide-key-sequence
-      '("e" "C-x" "SPC" "C-c" "e RET" "C-x RET" "e C-n" "C-x n"))
-
-;; Don't show the minor mode in the modeline.
-(require 'conf/modeline/cleaner-minor-modes)
-(diminish 'guide-key-mode "")
+(use-package guide-key
+  :ensure guide-key
+  :diminish guide-key-mode
+  :commands guide-key-mode
+  :init
+  (guide-key-mode 1)
+  :config
+  (setq guide-key/idle-delay 0.5)
+  (setq guide-key/recursive-key-sequence-flag t) ; If `guide-key' is enabled for a keymap, also enable it for its subkeymaps.
+  (setq guide-key/guide-key-sequence
+        '("e" "C-x"
+          "SPC" "C-c"
+          "e RET" "C-x RET"
+          "e C-n" "C-x n")))
 
 (provide 'conf/other/guide-key)
