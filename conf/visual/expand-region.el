a4a2f2cff7eff95e4a637b642371a96074dc28e7
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-22 18:11:17 +0200

    Convert conf/visual/expand-region.el to use-package

--- conf/visual/expand-region.el
+++ conf/visual/expand-region.el
@@ -1,16 +1,19 @@
 ;;; Expand-region: increase selection by semantic units.
 
+(require 'conf/packages)
 (require 'conf/evil)
 
-(require 'conf/packages)
-(package-ensure-installed 'expand-region)
+(use-package expand-region
+  :ensure expand-region
+  :commands (er/expand-region er/contract-region)
+  :init
 
-;; Bindings: C-SPC -- expand, C-M-SPC -- contract.
-(define-key evil-motion-state-map (kbd "C-SPC") 'er/expand-region)
-(define-key evil-motion-state-map (kbd "C-M-SPC") 'er/contract-region)
+  ;; Bindings: C-SPC -- expand, C-M-SPC -- contract.
+  (define-key evil-motion-state-map (kbd "C-SPC") #'er/expand-region)
+  (define-key evil-motion-state-map (kbd "C-M-SPC") #'er/contract-region)
 
-;; Bindings for terminals C-@ -- expand, C-M-@ -- contract.
-(define-key evil-motion-state-map (kbd "C-@") 'er/expand-region)
-(define-key evil-motion-state-map (kbd "C-M-@") 'er/contract-region)
+  ;; Bindings for terminals C-@ -- expand, C-M-@ -- contract.
+  (define-key evil-motion-state-map (kbd "C-@") #'er/expand-region)
+  (define-key evil-motion-state-map (kbd "C-M-@") #'er/contract-region))
 
 (provide 'conf/visual/expand-region)
