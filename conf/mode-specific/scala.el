4f989d73cb6f807b1ce8ee5471dceb7dfa3f28aa
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-22 09:07:20 +0200

    Convert conf/mode-specific/scala.el to use-package

--- conf/mode-specific/scala.el
+++ conf/mode-specific/scala.el
@@ -1,21 +1,24 @@
 ;;; Scala.
 
-;; Scala mode 2 -- for Scala 2.9+.
 (require 'conf/packages)
-(package-ensure-installed 'scala-mode2)
-
-;; Indentation (Smart Tabs).
 (require 'conf/editing/indentation)
-(smart-tabs-add-language-support scala scala-mode-hook
-  ((scala-indent:indent-line . scala-indent:step)))
-(smart-tabs-insinuate 'scala)
-(add-hook 'scala-mode-hook #'enable-indent-tabs-mode)
-
 (require 'conf/utils/repl) ; Used: repl-in-term.
-(defun scala-repl ()
-  "Launch a Scala REPL in another window.
+
+(use-package scala-mode2 ; Scala mode 2 -- for Scala 2.9+.
+  :ensure scala-mode2
+  :defer t
+  :config
+
+  ;; Indentation (Smart Tabs).
+  (smart-tabs-add-language-support scala scala-mode-hook
+    ((scala-indent:indent-line . scala-indent:step)))
+  (smart-tabs-insinuate 'scala)
+  (add-hook 'scala-mode-hook #'enable-indent-tabs-mode)
+
+  (defun scala-repl ()
+    "Launch a Scala REPL in another window.
 If the current buffer is in Scala mode, `isend-associate' it with the REPL."
-  (interactive)
-  (repl-in-term "scala-repl" "scala" (eq major-mode 'scala-mode)))
+    (interactive)
+    (repl-in-term "scala-repl" "scala" (eq major-mode 'scala-mode))))
 
 (provide 'conf/mode-specific/scala)
