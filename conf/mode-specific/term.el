dea5cd74a4fb0b84b56cfd0987ad0d686509b6d9
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-22 09:07:32 +0200

    Convert conf/mode-specific/term.el to use-package

--- conf/mode-specific/term.el
+++ conf/mode-specific/term.el
@@ -1,9 +1,12 @@
 ;;; Terminal modes.
 
-(add-hook 'term-mode-hook #'my-term-mode-hook)
+(use-package term ; Bundled with Emacs.
+  :defer t
+  :config
 
-(defun my-term-mode-hook ()
-  (hl-line-mode -1) ; Don't highlight the current line.
-  (setq scroll-margin 0)) ; Some programs display prompts at the bottom of the screen.)
+  (defun my-term-mode-hook ()
+    (hl-line-mode -1) ; Don't highlight the current line.
+    (setq scroll-margin 0)) ; Some programs display prompts at the bottom of the screen.
+  (add-hook 'term-mode-hook #'my-term-mode-hook))
 
 (provide 'conf/mode-specific/term)
