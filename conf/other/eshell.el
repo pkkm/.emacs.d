f46b5177b981b7c070c7ca799a0769efbfa00c6c
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-22 18:05:10 +0200

    Convert conf/other/eshell.el to use-package

--- conf/other/eshell.el
+++ conf/other/eshell.el
@@ -1,7 +1,11 @@
 ;;; Eshell -- Emacs shell.
 
-;; Keep savefiles in `my-savefile-dir'/eshell.
-(setq eshell-directory-name
-      (expand-file-name "eshell" my-savefile-dir))
+(use-package eshell ; Bundled with Emacs.
+  :defer t
+  :config
+
+  ;; Keep savefiles in `my-savefile-dir'/eshell.
+  (setq eshell-directory-name
+        (expand-file-name "eshell" my-savefile-dir)))
 
 (provide 'conf/other/eshell)
