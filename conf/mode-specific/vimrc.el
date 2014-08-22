038ad36ae15b6689d5e9375e184614a3e7e270d4
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-22 15:26:50 +0200

    Convert conf/mode-specific/vimrc.el to use-package

--- conf/mode-specific/vimrc.el
+++ conf/mode-specific/vimrc.el
@@ -1,11 +1,11 @@
 ;;; Vim configuration files (also useful for pentadactylrc, etc.).
 
 (require 'conf/packages)
-(package-ensure-installed 'vimrc-mode)
 
-;; Automaticaly activate for *.vim, vimrc, .pentadactylrc.
-(add-to-list 'auto-mode-alist '(".vim\\'" . vimrc-mode))
-(add-to-list 'auto-mode-alist '("vimrc\\'" . vimrc-mode))
-(add-to-list 'auto-mode-alist '(".pentadactylrc\\'" . vimrc-mode))
+(use-package vimrc-mode
+  :ensure vimrc-mode
+  :mode ((".vim\\'" . vimrc-mode)
+         ("vimrc\\'" . vimrc-mode)
+         (".pentadactylrc\\'" . vimrc-mode)))
 
 (provide 'conf/mode-specific/vimrc)
