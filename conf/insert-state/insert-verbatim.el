bcb2d798862b47cab7194e6e8f4b61a84018a4e6
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-22 18:12:48 +0200

    Quote functions with #'

--- conf/insert-state/insert-verbatim.el
+++ conf/insert-state/insert-verbatim.el
@@ -2,7 +2,7 @@
 
 (require 'conf/evil)
 
-(define-key evil-insert-state-map (kbd "C-v") 'quoted-insert)
-(define-key evil-replace-state-map (kbd "C-v") 'quoted-insert)
+(define-key evil-insert-state-map (kbd "C-v") #'quoted-insert)
+(define-key evil-replace-state-map (kbd "C-v") #'quoted-insert)
 
 (provide 'conf/insert-state/insert-verbatim)
