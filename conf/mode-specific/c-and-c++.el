800e527b3533510889ba10f50dc428577b17c318
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-22 17:21:48 +0200

    Don't do anything if not visiting a file in set-c-compile-run-clean-commands

--- conf/mode-specific/c-and-c++.el
+++ conf/mode-specific/c-and-c++.el
@@ -26,24 +26,25 @@
   ;; Default compilation, execution and cleaning commands.
   (defun set-c-compile-run-clean-commands ()
     (interactive)
-    (let* ((input-file-name (file-name-nondirectory (buffer-file-name)))
-           (output-file-name (concat (file-name-sans-extension input-file-name)
-                                     (if (eq window-system 'w32) ".exe" "")))
-           (input-file-name-quoted (shell-quote-argument input-file-name))
-           (output-file-name-quoted (shell-quote-argument output-file-name))
-           (compiler "gcc"))
-      (set (make-local-variable 'compile-command)
-           (concat compiler " " input-file-name-quoted " -o " output-file-name-quoted
-                   (if (string= my-additional-compile-args "") "" " ")
-                   my-additional-compile-args ; Defined in 'conf/other/compiling. To be used as a file-local variable.
-                   " --std=c99 -O2" ; Compile using the C99 standard and optimize.
-                   " -Wall -Wextra" ; Essential warnings.
-                   " -Werror=implicit-function-declaration" ; Calling an undefined function should be an error.
-                   " -Wstrict-overflow=5" ; Warn on possible signed overflow.
-                   " -ftrapv" ; Add runtime checks for undefined behavior (hurts performance).
-                   (if (string= compiler "clang") " -fsanitize=undefined-trap -fsanitize-undefined-trap-on-error" ""))) ; Clang-specific runtime undefined behavior checks.
-      (set (make-local-variable 'run-command) (concat "./" output-file-name-quoted))
-      (set (make-local-variable 'clean-command) (concat "rm " output-file-name-quoted))))
+    (when (buffer-file-name)
+      (let* ((input-file-name (file-name-nondirectory (buffer-file-name)))
+             (output-file-name (concat (file-name-sans-extension input-file-name)
+                                       (if (eq window-system 'w32) ".exe" "")))
+             (input-file-name-quoted (shell-quote-argument input-file-name))
+             (output-file-name-quoted (shell-quote-argument output-file-name))
+             (compiler "gcc"))
+        (set (make-local-variable 'compile-command)
+             (concat compiler " " input-file-name-quoted " -o " output-file-name-quoted
+                     (if (string= my-additional-compile-args "") "" " ")
+                     my-additional-compile-args ; Defined in 'conf/other/compiling. To be used as a file-local variable.
+                     " --std=c99 -O2" ; Compile using the C99 standard and optimize.
+                     " -Wall -Wextra" ; Essential warnings.
+                     " -Werror=implicit-function-declaration" ; Calling an undefined function should be an error.
+                     " -Wstrict-overflow=5" ; Warn on possible signed overflow.
+                     " -ftrapv" ; Add runtime checks for undefined behavior (hurts performance).
+                     (if (string= compiler "clang") " -fsanitize=undefined-trap -fsanitize-undefined-trap-on-error" ""))) ; Clang-specific runtime undefined behavior checks.
+        (set (make-local-variable 'run-command) (concat "./" output-file-name-quoted))
+        (set (make-local-variable 'clean-command) (concat "rm " output-file-name-quoted)))))
   (add-hook 'c-mode-hook #'set-c-compile-run-clean-commands))
 
 (provide 'conf/mode-specific/c-and-c++)
