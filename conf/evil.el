92604dd4f4085d2e0c46d610f0af1bf2dc6961b5
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-22 08:39:25 +0200

    Convert conf/evil.el to use-package

--- conf/evil.el
+++ conf/evil.el
@@ -1,61 +1,64 @@
 ;;; Evil (Extensible VI Layer).
 
-;; Make Evil's > and < always indent by `tab-width'.
-;; This has to happen before loading Evil, because Evil makes `evil-shift-width' buffer local, and therefore impossible to declare as an alias.
-;; I haven't found a way to reverse making variable buffer-local.
-(defvaralias 'evil-shift-width 'tab-width)
+(use-package evil
+  :ensure evil
+  :diminish undo-tree-mode
+  :pre-load
+  ;; Make Evil's > and < always indent by `tab-width'.
+  ;; This has to happen before loading Evil, because Evil makes `evil-shift-width' buffer local, and therefore impossible to declare as an alias.
+  ;; I haven't found a way to reverse making variable buffer-local.
+  (defvaralias 'evil-shift-width 'tab-width)
+  :config
 
-(require 'conf/packages)
-(package-ensure-installed 'evil)
+  (evil-mode 1)
 
-(evil-mode 1)
+  (setq evil-cross-lines t) ; Allow motions to cross newlines.
 
-(setq evil-cross-lines t) ; Allow motions to cross newlines.
+  ;; Search.
+  (setq evil-regexp-search nil) ; Don't use regexes for / and ?.
+  (setq evil-flash-delay 999999) ; Number of seconds to highlight matches for.
 
-(setq evil-regexp-search nil) ; Don't use regexes for / and ?.
-(setq evil-flash-delay 999999) ; Number of seconds to highlight matches for.
+  (setq evil-echo-state nil) ; Don't echo state in the echo area (minibuffer).
 
-;; Make C-u scroll instead of starting an argument.
-(define-key evil-motion-state-map (kbd "C-u") 'evil-scroll-up)
 
-;; In insert state, auto-indent the new line on a press of "RET" (or similar).
-(dolist (fun-to-remap
-         '(newline newline-and-indent evil-ret))
-  (define-key evil-insert-state-map (vector 'remap fun-to-remap) #'evil-ret-and-indent))
+  ;;; Keybindings.
 
-(setq evil-want-C-w-delete t) ; Delete word in insert state with C-w.
+  ;; In insert state, auto-indent the new line on a press of "RET" (or similar).
+  (dolist (fun-to-remap
+           '(newline newline-and-indent evil-ret))
+    (define-key evil-insert-state-map (vector 'remap fun-to-remap) #'evil-ret-and-indent))
 
-(setq evil-echo-state nil) ; Don't echo state in the echo area (minibuffer).
+  ;; Make C-u scroll instead of starting an argument.
+  (bind-key "C-u" #'evil-scroll-up evil-motion-state-map) ; Setting `evil-want-C-u-scroll' doesn't work.
 
-;; Don't allow modes to change the definition of paragraph used by { and }.
-(defun kill-local-paragraph-definitions ()
-  "Kill the local values of `paragraph-start' and `paragraph-separate'."
-  (kill-local-variable 'paragraph-start)
-  (kill-local-variable 'paragraph-separate))
-(add-hook 'after-change-major-mode-hook #'kill-local-paragraph-definitions)
+  (setq evil-want-C-w-delete t) ; Delete word in insert state with C-w.
 
-;; Initial states in various modes.
-;; TODO move to files with the configuration for the modes.
-(evil-set-initial-state 'debugger-mode 'normal)
-(evil-set-initial-state 'package-menu-mode 'normal)
-(evil-set-initial-state 'compilation-mode 'motion)
-(evil-set-initial-state 'occur-mode 'motion)
-(evil-set-initial-state 'term-mode 'emacs)
-(evil-set-initial-state 'compilation-mode 'emacs)
+  ;; Unset keys that I'll later use as prefix keys.
+  (bind-key "SPC" nil evil-motion-state-map)
 
-;; Don't allow any keymap to shadow Evil bindings.
-(setq evil-overriding-maps '()) ; `evil-overriding-maps' get elevated to the "overriding" status -- above global state keymap, but below the local one.
-(setq evil-pending-overriding-maps '()) ; No idea what the "pending" version is for, but setting only the above doesn't seem to work, for example in compilation-mode.
-;;(setq evil-intercept-maps '()) ; `evil-intercept-maps' get elevated to the "intercept" status -- above all others. This is useful when debugging.
+  ;; Unset other useless keys.
+  (bind-key "C-e" nil evil-insert-state-map)
 
-;; Unset keys that I'll later use as prefix keys.
-(define-key evil-motion-state-map (kbd "SPC") nil)
+  ;; Don't allow any keymap to shadow Evil bindings.
+  (setq evil-overriding-maps '()) ; `evil-overriding-maps' get elevated to the "overriding" status -- above global state keymap, but below the local one.
+  (setq evil-pending-overriding-maps '()) ; No idea what the "pending" version is for, but setting only the above doesn't seem to work, for example in compilation-mode.
+  ;;(setq evil-intercept-maps '()) ; `evil-intercept-maps' get elevated to the "intercept" status -- above all others. This is useful when debugging.
 
-;; Unset other useless keys.
-(define-key evil-insert-state-map (kbd "C-e") nil)
 
-;; Don't display undo-tree-mode (used by evil) in the modeline.
-(require 'conf/modeline/cleaner-minor-modes)
-(diminish 'undo-tree-mode "")
+  ;; Don't allow modes to change the definition of paragraph used by { and }.
+  (defun kill-local-paragraph-definitions ()
+    "Kill the local values of `paragraph-start' and `paragraph-separate'."
+    (kill-local-variable 'paragraph-start)
+    (kill-local-variable 'paragraph-separate))
+  (add-hook 'after-change-major-mode-hook #'kill-local-paragraph-definitions)
+
+  ;; Initial states in various modes.
+  ;; TODO move to files with the configuration for the modes.
+  (evil-set-initial-state 'debugger-mode 'normal)
+  (evil-set-initial-state 'package-menu-mode 'normal)
+  (evil-set-initial-state 'compilation-mode 'motion)
+  (evil-set-initial-state 'occur-mode 'motion)
+  (evil-set-initial-state 'term-mode 'emacs)
+  (evil-set-initial-state 'compilation-mode 'emacs))
 
 (provide 'conf/evil)
