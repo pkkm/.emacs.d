e6662b1fcc372f35f56c47283cf5ee53ebbcff6f
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-26 17:25:09 +0200

    Show init time in the minibufer instead of *scratch*

--- conf/view/initial-messages.el
+++ conf/view/initial-messages.el
@@ -7,11 +7,13 @@
       (add-to-list var '(initialization))
     (set var '((initialization)))))
 
-;; Display the init time in the *scratch* buffer.
-(defun display-init-time-in-scratch-buffer ()
-  (setq initial-scratch-message
-        (format ";; Init time: %s.\n\n" (emacs-init-time))))
-(add-hook 'after-init-hook #'display-init-time-in-scratch-buffer)
+;; Start with an empty *scratch* buffer.
+(setq initial-scratch-message nil)
+
+;; Show init time in the minibuffer.
+(defun show-emacs-init-time ()
+  (message "Init time: %s." (emacs-init-time)))
+(add-hook 'emacs-startup-hook #'show-emacs-init-time)
 
 ;; Don't display the startup screen.
 (setq inhibit-startup-message t)
