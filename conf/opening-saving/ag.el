f317fd54e42ff2eb53848f159f2bcab1363c9573
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-26 19:50:35 +0200

    Convert conf/opening-saving/ag.el to use-package

--- conf/opening-saving/ag.el
+++ conf/opening-saving/ag.el
@@ -1,11 +1,11 @@
 ;;; Ag -- the silver searcher (alternative to ack, which is an alternative to grep).
 ;; Usage: `ag', `ag-regexp', `ag-project', `ag-project-regexp'.
 
-(package-ensure-installed 'ag)
-
-(setq ag-highlight-search t)
-
-(global-set-key (kbd "C-c a") #'ag)
-(global-set-key (kbd "C-c A") #'ag-project)
+(use-package ag
+  :ensure ag
+  :bind (("C-c a" . ag)
+         ("C-c A" . ag-project))
+  :config
+  (setq ag-highlight-search t))
 
 (provide 'conf/opening-saving/ag)
