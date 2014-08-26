4a844437179ebccec5e0231a03baa9132a08e4b7
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-26 17:49:36 +0200

    Convert conf/editing/indentation.el to use-package; refactor

--- conf/editing/indentation.el
+++ conf/editing/indentation.el
@@ -1,8 +1,5 @@
 ;;; Indentation.
 
-;; By default, use spaces and mode's default configuration.
-(setq-default indent-tabs-mode nil)
-
 ;; For specific languages, use Smart Tabs -- indent with tabs, align with spaces.
 ;; To enable for a language:
 ;;   (smart-tabs-insinuate 'language)
@@ -11,8 +8,16 @@
 ;;   (smart-tabs-add-language-support c++ c++-mode-hook
 ;;     ((c-indent-line . c-basic-offset)
 ;;      (c-indent-region . c-basic-offset)))
-(package-ensure-installed 'smart-tabs-mode)
-(require 'smart-tabs-mode) ; TODO needed?
+(use-package smart-tabs-mode
+  :ensure smart-tabs-mode
+  :defer t)
+
+;; By default, indent with 4 spaces.
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+;; Don't convert tabs to spaces on backspace.
+(setq backward-delete-char-untabify-method nil)
 
 ;; Utility functions for easy adding to hooks.
 (defun enable-indent-tabs-mode ()
@@ -22,20 +27,14 @@
   "Set `indent-tabs-mode' to t in the current buffer."
   (setq indent-tabs-mode nil))
 
-(setq-default tab-width 4)
-
 ;; Make a tabstop every `tab-width' characters.
-;; (Tabstops -- positions to align to in the Fundamental mode.)
-;; (Doesn't affect programming modes.)
+;; (Tabstops -- positions to align to in the Fundamental mode. Doesn't affect programming modes.)
 (defun my-generate-tab-stops (&optional width max)
   "Return a sequence suitable for `tab-stop-list'."
   (let* ((tab-width (or width tab-width))
-		 (max-column (or max 200))
+         (max-column (or max 200))
          (count (/ max-column tab-width)))
     (number-sequence tab-width (* tab-width count) tab-width)))
 (setq tab-stop-list (my-generate-tab-stops))
-
-;; Don't convert tabs to spaces on backspace.
-(setq backward-delete-char-untabify-method nil)
 
 (provide 'conf/editing/indentation)
