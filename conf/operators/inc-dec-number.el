efad092d583e3da3bd49a21b38fda86ba63f0508
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-27 12:57:32 +0200

    Convert conf/operators/inc-dec-number.el to use-package

--- conf/operators/inc-dec-number.el
+++ conf/operators/inc-dec-number.el
@@ -1,9 +1,10 @@
 ;;; Increment/decrement the number at point.
 
-(package-ensure-installed 'evil-numbers)
-
-(with-eval-after-load 'evil
-  (define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
-  (define-key evil-normal-state-map (kbd "C-M-a") 'evil-numbers/dec-at-pt))
+(use-package evil-numbers
+  :defer t
+  :init
+  (with-eval-after-load 'evil
+    (bind-key "C-a" #'evil-numbers/inc-at-pt evil-normal-state-map)
+    (bind-key "C-M-a" #'evil-numbers/dec-at-pt evil-normal-state-map)))
 
 (provide 'conf/operators/inc-dec-number)
