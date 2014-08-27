45f2a6420ab4918d5de6b91397e632c98707537f
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-27 12:45:38 +0200

    Convert conf/opening-saving/pastebin.el to use-package

--- conf/opening-saving/pastebin.el
+++ conf/opening-saving/pastebin.el
@@ -1,24 +1,29 @@
 ;;; Support for the paste.debian.net pastebin.
 
-(package-ensure-installed 'debpaste)
+(use-package debpaste
+  :ensure debpaste
+  :defer t
+  :init
 
-;; C-c p -- paste region or buffer.
-(defun my-debpaste-dwim ()
-  "If the region is active, call `debpaste-paste-region'. Otherwise, `debpaste-paste-buffer'."
-  (interactive)
-  (if (use-region-p)
-      (call-interactively #'debpaste-paste-region)
-    (call-interactively #'debpaste-paste-buffer)))
-(global-set-key (kbd "C-c p") #'my-debpaste-dwim)
+  ;; C-c p -- paste region or buffer.
+  (defun my-debpaste-dwim ()
+    "If the region is active, call `debpaste-paste-region'. Otherwise, `debpaste-paste-buffer'."
+    (interactive)
+    (if (use-region-p)
+        (call-interactively #'debpaste-paste-region)
+      (call-interactively #'debpaste-paste-buffer)))
+  (bind-key "C-c p" #'my-debpaste-dwim)
 
-;; C-c P i -- display the info associated with last posted paste.
-;; C-c P d -- delete the paste with a given hash (hash under point is used if available.)
-(global-set-key (kbd "C-c P i") #'debpaste-display-posted-info-in-buffer)
-(global-set-key (kbd "C-c P d") #'debpaste-delete-paste)
+  ;; C-c P i -- display the info associated with last posted paste.
+  ;; C-c P d -- delete the paste with a given hash (hash under point is used if available.)
+  :bind (("C-c P i" . debpaste-display-posted-info-in-buffer)
+         ("C-c P d" . debpaste-delete-paste))
 
-;; Prompt for expiration time and paste language.
-(setq debpaste-prompted-paste-options '(lang expire-sec))
+  :config
 
-(setq debpaste-user-name "pkkm")
+  ;; Prompt for expiration time and paste language.
+  (setq debpaste-prompted-paste-options '(lang expire-sec))
+
+  (setq debpaste-user-name "pkkm"))
 
 (provide 'conf/opening-saving/pastebin)
