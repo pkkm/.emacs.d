cfcf6ad923703d89500bc1b924d04545dc3e2e99
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-27 13:18:47 +0200

    Replace startup message with the init time
    The previous solution was to display the init time after the startup
    message.

--- conf/view/initial-messages.el
+++ conf/view/initial-messages.el
@@ -10,10 +10,9 @@
 ;; Start with an empty *scratch* buffer.
 (setq initial-scratch-message nil)
 
-;; Show init time in the minibuffer.
-(defun show-emacs-init-time ()
-  (message "Init time: %s." (emacs-init-time)))
-(add-hook 'emacs-startup-hook #'show-emacs-init-time)
+;; Show init time in the minibuffer instead of the default message.
+(defun startup-echo-area-message ()
+  (format "Init time: %s." (emacs-init-time)))
 
 ;; Don't display the startup screen.
 (setq inhibit-startup-message t)
