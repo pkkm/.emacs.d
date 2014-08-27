d76228678ca184076561dad734ccd21cc8745ea8
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-27 12:51:15 +0200

    Convert conf/opening-saving/editorconfig.el to use-package

--- conf/opening-saving/editorconfig.el
+++ conf/opening-saving/editorconfig.el
@@ -1,8 +1,7 @@
 ;;; Support for the EditorConfig format for defining coding styles (see editorconfig.org).
 
-(package-ensure-installed 'editorconfig)
-
-;; The package should be initialized automatically.
-;; (The `edconf-find-file-hook' function will be added to `find-file-hook'.)
+(use-package editorconfig
+  :ensure editorconfig
+  :defer t) ; The package will be initialized automatically. (The `edconf-find-file-hook' function will be added to `find-file-hook'.)
 
 (provide 'conf/opening-saving/editorconfig)
