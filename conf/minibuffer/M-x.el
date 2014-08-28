c50fcc1c7c57e6d1fa76837872e5bc7e6d23b077
Paweł Kraśnicki <dev@pkkm.eu>
2014-08-29 00:13:05 +0200

    Don't save smex data in my home directory

--- conf/minibuffer/M-x.el
+++ conf/minibuffer/M-x.el
@@ -4,7 +4,10 @@
   :ensure smex
   :defer t
   :bind (("M-x" . smex)
-         ("C-x m" . smex)))
+         ("C-x m" . smex))
+  :config
+  ;; Save smex data in `user-emacs-directory' instead of my home directory.
+  (setq smex-save-file (locate-user-emacs-file "smex")))
 
 ;; Unbound, interesting Smex commands:
 ;;  * smex-major-mode-commands -- limit to the commands relevant to the active major mode (try with Dired or Magit).
