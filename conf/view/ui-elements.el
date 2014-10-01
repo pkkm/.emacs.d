3c6a47e50bb676cc2001333ebe90926cd3488fa8
Paweł Kraśnicki <dev@pkkm.eu>
2014-10-01 19:11:08 +0200

    Compatibility with Emacs 24.3

--- conf/view/ui-elements.el
+++ conf/view/ui-elements.el
@@ -2,7 +2,9 @@
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
+
 (scroll-bar-mode -1)
-(horizontal-scroll-bar-mode -1)
+(when (fboundp 'horizontal-scroll-bar-mode)
+  (horizontal-scroll-bar-mode -1))
 
 (provide 'conf/view/ui-elements)
