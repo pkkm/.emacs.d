e814b916d7d53c7f5756fb1ba832250bf0c1073b
Paweł Kraśnicki <dev@pkkm.eu>
2014-10-03 23:15:36 +0200

    Use with-eval-after-load in conf/motions/marks-registers.el

--- conf/motions/marks-registers.el
+++ conf/motions/marks-registers.el
@@ -1,12 +1,13 @@
 ;;; Jumping to mark/register.
 ;; Emacs registers are global, in contrast to Evil's buffer-local markers. They also can be used for storing window configurations, file names, etc.
 
-;; ' -- go to a mark (instead of the mark's line).
-(bind-key "'" #'evil-goto-mark evil-motion-state-map)
+(with-eval-after-load 'evil
+  ;; ' -- go to a mark (instead of the mark's line).
+  (bind-key "'" #'evil-goto-mark evil-motion-state-map)
 
-;; M -- store the point position in an Emacs register
-;; ` -- jump to a register.
-(bind-key "M" #'point-to-register evil-motion-state-map)
-(bind-key "`" #'jump-to-register evil-motion-state-map)
+  ;; M -- store the point position in an Emacs register
+  ;; ` -- jump to a register.
+  (bind-key "M" #'point-to-register evil-motion-state-map)
+  (bind-key "`" #'jump-to-register evil-motion-state-map))
 
 (provide 'conf/motions/marks-registers)
