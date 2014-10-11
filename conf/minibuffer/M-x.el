d3816a9ddc3bedef94fcaeb74af19f7f6c914c40
Paweł Kraśnicki <dev@pkkm.eu>
2014-10-11 21:58:35 +0200

    Add C-x SPC (e SPC) binding for smex

--- conf/minibuffer/M-x.el
+++ conf/minibuffer/M-x.el
@@ -4,13 +4,12 @@
   :ensure smex
   :defer t
   :bind (("M-x" . smex)
-         ("C-x m" . smex))
+         ("C-x SPC" . smex)
+         ("C-x m" . smex)) ; TODO change this to `smex-major-mode-commands' when I get used to the C-x SPC binding.
   :config
   ;; Save smex data in `user-emacs-directory' instead of my home directory.
   (setq smex-save-file (locate-user-emacs-file "smex")))
 
-;; Unbound, interesting Smex commands:
-;;  * smex-major-mode-commands -- limit to the commands relevant to the active major mode (try with Dired or Magit).
-;;  * smex-show-unbound-commands -- show frequently called commands that are unbound.
+;; Interesting command: smex-show-unbound-commands -- show frequently called commands that are unbound.
 
 (provide 'conf/minibuffer/M-x)
