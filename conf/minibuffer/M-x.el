abfdb765c1a7125a7993e35ec6fdd70c92ee45c8
Paweł Kraśnicki <dev@pkkm.eu>
2014-11-07 21:41:29 +0100

    Make C-x m execute commands of the current major mode

--- conf/minibuffer/M-x.el
+++ conf/minibuffer/M-x.el
@@ -5,9 +5,9 @@
   :defer t
   :bind (("M-x" . smex)
          ("C-x SPC" . smex)
-         ("C-x m" . smex)) ; TODO change this to `smex-major-mode-commands' when I get used to the C-x SPC binding.
+         ("C-x m" . smex-major-mode-commands))
   :config
-  ;; Save smex data in `user-emacs-directory' instead of my home directory.
+  ;; Save smex data in `user-emacs-directory' instead of the home directory.
   (setq smex-save-file (locate-user-emacs-file "smex")))
 
 ;; Interesting command: smex-show-unbound-commands -- show frequently called commands that are unbound.
