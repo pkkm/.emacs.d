9b5cb6e595cc3eda882527c83339cdcea6b836bd
Paweł Kraśnicki <dev@pkkm.eu>
2014-12-03 23:43:30 +0100

    Expand region with v in visual state (instead of C-SPC)

--- conf/region/expand-region.el
+++ conf/region/expand-region.el
@@ -1,4 +1,4 @@
-;;; Expand-region: increase selection by semantic units.
+;;; Expand-region: expant/contract selection by semantic units.
 
 (use-package expand-region
   :ensure expand-region
@@ -6,12 +6,14 @@
   :init
 
   (with-eval-after-load 'evil
-    ;; Bindings: C-SPC -- expand, C-M-SPC -- contract.
-    (bind-key "C-SPC" #'er/expand-region evil-motion-state-map)
-    (bind-key "C-M-SPC" #'er/contract-region evil-motion-state-map)
+    (evil-define-command evil-visual-char-or-expand-region ()
+      "Switch to character (default) selection in Evil visual state. If we're already in it, expand region."
+      :keep-visual t ; Keep point where it is instead of moving it to the beginning/end of visual selection. Otherwise, switching from visual line to visual char would move the point.
+      (if (eq evil-visual-selection 'char)
+          (call-interactively #'er/expand-region)
+        (call-interactively #'evil-visual-char)))
 
-    ;; The same for terminals, where C-SPC sends C-@.
-    (bind-key "C-@" #'er/expand-region evil-motion-state-map)
-    (bind-key "C-M-@" #'er/contract-region evil-motion-state-map)))
+    (bind-key "v" #'evil-visual-char-or-expand-region evil-visual-state-map)
+    (bind-key "M-v" #'er/contract-region evil-visual-state-map)))
 
 (provide 'conf/region/expand-region)
