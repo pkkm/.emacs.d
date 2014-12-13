f38b9f6f1a74493e624102d9220154b58bf70b66
Paweł Kraśnicki <dev@pkkm.eu>
2014-12-13 15:40:52 +0100

    Revert "Add smartscan-mode (jumping to next/prev symbol)"
    This reverts commit 2cc8ec13a3fb78ba2c88b32c5de08e07755c83de.
    
    The M-n and M-p bindings of smartscan-mode interfered with shells and
    REPLs.

--- conf/main.el
+++ conf/main.el
@@ -43,7 +43,6 @@
 (require 'conf/region/undo-in-region)
 
 (require 'conf/moving/little-word)
-(require 'conf/moving/prev-next-symbol)
 (require 'conf/moving/ace-jump)
 
 (require 'conf/editing/indentation)
