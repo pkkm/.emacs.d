73f47d08f5c18c32343cbe579e5bf1c6d6df4108
Paweł Kraśnicki <dev@pkkm.eu>
2014-12-30 15:02:24 +0100

    Workaround for error in helm-semantic-or-imenu

--- conf/minibuffer/helm.el
+++ conf/minibuffer/helm.el
@@ -4,6 +4,8 @@
   :ensure helm
   :defer t
   :init
-  (bind-key "C-x g" #'helm-semantic-or-imenu)) ; Go to some function/variable definition (works with a lot of modes).
+  (bind-key "C-x g" #'helm-semantic-or-imenu) ; Go to some function/variable definition (works with a lot of modes).
+  :config
+  (require 'helm-plugin)) ; Fixes error in `helm-semantic-or-imenu': "helm-interpret-value: Symbol must be a function or a variable".
 
 (provide 'conf/minibuffer/helm)
