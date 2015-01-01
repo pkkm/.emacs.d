f51d2fcaa40671cab5ebcb930fa2dc24583098a9
Paweł Kraśnicki <dev@pkkm.eu>
2015-01-01 14:54:17 +0100

    Refactor

--- conf/view/gui/start-maximized.el
+++ conf/view/gui/start-maximized.el
@@ -2,9 +2,8 @@
 
 (defun maximize-frame-if-visible ()
   "Maximize the current frame if it's visible (not minimized)."
-  (when (and (eq (frame-visible-p (selected-frame)) t)
-             (not (or (eq (frame-parameter nil 'fullscreen) 'maximized)
-                      (eq (frame-parameter nil 'maximized) 'maximized))))
+  (unless (memq 'maximized (list (frame-parameter nil 'fullscreen)
+                                 (frame-parameter nil 'maximized)))
     (cond
      ((fboundp 'toggle-frame-maximized) (toggle-frame-maximized))
      ((eq window-system 'w32) (w32-send-sys-command 61488)))))
