bd7a4247afaf7e4b862fee1f0e4806ae7049ee9e
Paweł Kraśnicki <dev@pkkm.eu>
2015-01-01 15:35:08 +0100

    Refactor

--- conf/view/gui/start-maximized.el
+++ conf/view/gui/start-maximized.el
@@ -2,11 +2,10 @@
 
 (defun maximize-frame-if-visible ()
   "Maximize the current frame if it's visible (not minimized)."
-  (unless (memq 'maximized (list (frame-parameter nil 'fullscreen)
-                                 (frame-parameter nil 'maximized)))
+  (unless (frame-parameter nil 'fullscreen)
     (cond
      ((fboundp 'toggle-frame-maximized) (toggle-frame-maximized))
-     ((eq window-system 'w32) (w32-send-sys-command 61488)))))
+     ((eq window-system 'w32) (w32-send-sys-command 61488))))) ; Compatibility with Emacs 24.3 (24.4 has `toggle-frame-maximized').
 
 (add-hook 'emacs-startup-hook #'maximize-frame-if-visible)
 
