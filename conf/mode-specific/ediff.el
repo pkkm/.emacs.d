46358e343bfbd30a405487827675b84e53e8aba5
Paweł Kraśnicki <dev@pkkm.eu>
2015-01-30 17:18:40 +0100

    Add ztree-diff (recursive directory merge)

--- conf/mode-specific/ediff.el
+++ conf/mode-specific/ediff.el
@@ -1,4 +1,5 @@
 ;;; Ediff -- a mode for merging files.
+;; When using Ediff, press h for help.
 
 (use-package ediff ; Bundled with Emacs.
   :defer t
@@ -9,5 +10,23 @@
 
   ;; Display the second file to the side instead of below the first one.
   (setq ediff-split-window-function #'split-window-horizontally))
+
+;; ZTree -- diff directory trees.
+;; Usage: M-x ztree-diff, then:
+;;   * SPC, RET -- open/close directory.
+;;   * x -- open/close directory recursively.
+;;   * Backspace -- jump to parent directory.
+;;   * RET -- start Ediff on file.
+;;   * SPC -- show diff of current file.
+;;   * TAB -- switch between panels.
+;;   * h -- toggle showing identical files.
+;;   * C -- copy to other panel.
+;;   * D -- delete.
+;;   * v -- view.
+;;   * r -- rescan file/directory.
+;;   * F5 -- rescan everything.
+(use-package ztree
+  :ensure ztree
+  :defer t)
 
 (provide 'conf/mode-specific/ediff)
