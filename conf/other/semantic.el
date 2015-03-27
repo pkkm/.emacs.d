a077d0dc8bd67a04d48b9559fe07be57db8a6695
Paweł Kraśnicki <dev@pkkm.eu>
2015-03-27 11:58:34 +0100

    Add srefactor (refactoring tool) on C-c RET

--- conf/other/semantic.el
+++ conf/other/semantic.el
@@ -20,4 +20,16 @@
 ;;   * semantic-ia-show-summary -- show type and some other information.
 ;;   * semantic-analyze-proto-impl-toggle -- toggle between declaration and implementation.
 
+;; Refactoring on C-c RET (Emacs 24.4+).
+(when (version<= "24.4" emacs-version)
+  (use-package srefactor
+    :ensure srefactor
+    :defer t
+    :init
+    (with-eval-after-load 'semantic
+      (bind-key "C-c RET" #'srefactor-refactor-at-point semantic-mode-map))
+    :config
+    (with-eval-after-load 'evil
+      (evil-set-initial-state 'srefactor-ui-menu-mode 'emacs))))
+
 (provide 'conf/other/semantic)
