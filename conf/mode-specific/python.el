51f29bcffd85bee437e1af7b822c93780bc5b299
Paweł Kraśnicki <dev@pkkm.eu>
2015-05-25 18:53:54 +0200

    Add Elpy (Python programming environment)

---
+++ conf/mode-specific/python.el
@@ -0,0 +1,18 @@
+;;; Python.
+
+(use-package elpy
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'python
+    (elpy-enable))
+  :config
+
+  (setq elpy-rpc-python-command "python3")
+  (setq elpy-rpc-backend "jedi") ; Better completion than Rope (as of 2015-05).
+
+  ;; Disable some modules.
+  (let ((disabled-modules '(elpy-module-flymake elpy-module-highlight-indentation)))
+    (setq elpy-modules (-difference elpy-modules disabled-modules))))
+
+(provide 'conf/mode-specific/python)
