f5cdc7df31c36580f2e917fbfcfa42713ca7dc47
Paweł Kraśnicki <dev@pkkm.eu>
2015-06-06 22:32:51 +0200

    Remove unnecessary with-eval-after-load

--- conf/other/alternate-modifier-entering.el
+++ conf/other/alternate-modifier-entering.el
@@ -1,7 +1,6 @@
 ;;; Alternate ways of entering modified keys unavailable in terminals (e.g. C-}).
 
-(with-eval-after-load 'evil
-  (bind-key "M-c" nil) ; Otherwise will be shadowed by `capitalize-word' (as of 2015-04).
-  (bind-key "M-c" #'event-apply-control-modifier function-key-map))
+(bind-key "M-c" nil) ; Otherwise will be shadowed by `capitalize-word' (as of 2015-04).
+(bind-key "M-c" #'event-apply-control-modifier function-key-map)
 
 (provide 'conf/other/alternate-modifier-entering)
