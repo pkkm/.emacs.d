12973c8b6f9fd85285a1bb969a551813c89da6fa
Paweł Kraśnicki <dev@pkkm.eu>
2015-08-25 14:49:49 +0200

    Replace my own code with the evil-visualstar package

--- conf/region/search-selected.el
+++ conf/region/search-selected.el
@@ -1,25 +1,9 @@
-;;; Search for the selected text.
+;;; Search for selected text with # and * in visual state.
 
-(defun search-text-between (begin end forward-p)
-  "Search for the text between BEGIN and END (character offsets), forward if FORWARD-P is t."
-  (let ((text-to-search (buffer-substring-no-properties begin end)))
-    (setq isearch-forward forward-p) ; Tell ISearch whether the last search was forward or backward.
-    (evil-search text-to-search forward-p)))
-
-(defun search-selection-forward (sel-begin sel-end)
-  "Search for the selected text forward."
-  (interactive "r") ; Needs a selection.
-  (deactivate-mark)
-  (search-text-between sel-begin sel-end t))
-
-(defun search-selection-backward (sel-begin sel-end)
-  "Search for the selected text backward."
-  (interactive "r") ; Needs a selection.
-  (deactivate-mark)
-  (search-text-between sel-begin sel-end nil))
-
-(with-eval-after-load 'evil
-  (bind-key "*" #'search-selection-forward evil-visual-state-map)
-  (bind-key "#" #'search-selection-backward evil-visual-state-map))
+(use-package evil-visualstar
+  :ensure t
+  :init
+  (with-eval-after-load 'evil
+    (global-evil-visualstar-mode)))
 
 (provide 'conf/region/search-selected)
