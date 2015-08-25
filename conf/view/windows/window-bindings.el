8d735d91733793355f631dbe7e39af0ef4eded0a
Paweł Kraśnicki <dev@pkkm.eu>
2015-08-25 13:16:09 +0200

    Base window bindings on Evil ones; move with hjks

--- conf/view/windows/window-bindings.el
+++ conf/view/windows/window-bindings.el
@@ -2,17 +2,17 @@
 
 ;; Track the most recently created window.
 (defvar most-recently-created-window nil
-  "The window that was most recently created. This variable gets set when `split-window-internal' is called.")
+  "The window that was most recently created.")
 (defadvice split-window-internal (after record-window activate)
   (setq most-recently-created-window ad-return-value))
 
 ;; Commands to delete the most recently created window.
 (defun delete-most-recently-created-window ()
-  "Delete the window that was most recently created."
+  "Delete the most recently created window."
   (interactive)
   (delete-window most-recently-created-window))
 (defun kill-most-recently-created-window-and-buffer ()
-  "Delete the window that was most recently created and kill the buffer that it was displaying."
+  "Delete most recently created window and kill the buffer that it was displaying."
   (interactive)
   (select-window most-recently-created-window)
   (kill-buffer-and-window))
@@ -20,63 +20,51 @@
 ;; Split window, focus it and display the next buffer in it.
 (defun my-split-window-right ()
   (interactive)
-  (select-window (call-interactively #'split-window-right))
+  (call-interactively #'evil-window-vsplit)
+  (select-window most-recently-created-window)
   (switch-to-buffer (other-buffer)))
 (defun my-split-window-below ()
   (interactive)
-  (select-window (call-interactively #'split-window-below))
+  (call-interactively #'evil-window-split)
+  (select-window most-recently-created-window)
   (switch-to-buffer (other-buffer)))
 
-(defvar my-window-map (make-sparse-keymap)
-  "Keymap for my window-related commands.")
-(define-prefix-command 'my-window-map)
 (with-eval-after-load 'evil
-  (bind-key "<tab>" 'my-window-map evil-motion-state-map)
-  (bind-key "TAB" 'my-window-map evil-motion-state-map))
+  (bind-key "<tab>" 'evil-window-map evil-motion-state-map)
+  (bind-key "TAB" 'evil-window-map evil-motion-state-map)
 
+  ;; Split (with count: leave COUNT lines in the initially-selected window).
+  (bind-key "d" #'my-split-window-right evil-window-map)
+  (bind-key "-" #'my-split-window-below evil-window-map)
 
-;; Split (with count: leave COUNT lines in the initially-selected window).
-(bind-key "d" #'my-split-window-right my-window-map)
-(bind-key "-" #'my-split-window-below my-window-map)
+  ;; Close.
+  (bind-key "K" #'evil-delete-buffer evil-window-map) ; Kill buffer and window.
+  (bind-key "C-c" #'delete-most-recently-created-window evil-window-map)
+  (bind-key "C-k" #'kill-most-recently-created-window-and-buffer evil-window-map)
 
-;; Close.
-(bind-key "o" #'delete-other-windows my-window-map)
-(bind-key "c" #'delete-window my-window-map)
-(bind-key "k" #'evil-delete-buffer my-window-map) ; Kill buffer and window.
-(bind-key "C" #'delete-most-recently-created-window my-window-map)
-(bind-key "K" #'kill-most-recently-created-window-and-buffer my-window-map)
+  ;; Next, previous.
+  (bind-key "TAB" #'evil-window-next evil-window-map)
+  (bind-key "<backtab>" #'evil-window-prev evil-window-map) ; <backtab> -- S-TAB.
 
-;; Next, previous.
-(bind-key "TAB" #'evil-window-next my-window-map)
-(bind-key "<backtab>" #'evil-window-prev my-window-map) ; <backtab> -- S-TAB.
+  ;; Select the window with the most recently used buffer.
+  (bind-key "SPC" #'evil-window-mru evil-window-map)
 
-;; Select the window with the most recently used buffer.
-(bind-key "SPC" #'evil-window-mru my-window-map)
+  ;; Navigate/move with hjks instead of hjkl.
+  (bind-key "s" #'evil-window-right evil-window-map)
+  (bind-key "S" #'evil-window-move-far-right evil-window-map)
 
-;; Move focus directionally.
-(bind-key "h" #'evil-window-left my-window-map)
-(bind-key "t" #'evil-window-down my-window-map)
-(bind-key "n" #'evil-window-up my-window-map)
-(bind-key "s" #'evil-window-right my-window-map)
+  ;; Resize.
+  (bind-key "C-h" #'evil-window-decrease-width evil-window-map)
+  (bind-key "C-j" #'evil-window-increase-height evil-window-map)
+  (bind-key "C-k" #'evil-window-decrease-height evil-window-map)
+  (bind-key "C-s" #'evil-window-increase-width evil-window-map)
 
-;; Move window.
-(bind-key "H" #'evil-window-move-far-left my-window-map)
-(bind-key "T" #'evil-window-move-very-bottom my-window-map)
-(bind-key "N" #'evil-window-move-very-top my-window-map)
-(bind-key "S" #'evil-window-move-far-right my-window-map)
+  ;; Save/restore window configuration from register.
+  (bind-key "m" #'window-configuration-to-register evil-window-map)
+  (bind-key "'" #'jump-to-register evil-window-map)
 
-;; Resize (with count: by COUNT columns/lines).
-(bind-key "C-h" #'evil-window-decrease-width my-window-map)
-(bind-key "C-t" #'evil-window-increase-height my-window-map)
-(bind-key "C-n" #'evil-window-decrease-height my-window-map)
-(bind-key "C-s" #'evil-window-increase-width my-window-map)
-
-;; Make windows equal in size.
-(bind-key "=" #'balance-windows my-window-map)
-
-;; Rotate (first -> second, ..., last -> first).
-(bind-key "r" #'evil-window-rotate-downwards my-window-map)
-(bind-key "R" #'evil-window-rotate-upwards my-window-map)
+  ;; Find file in a new window (useful because it starts in the current window's directory).
+  (bind-key "f" #'find-file-other-window evil-window-map))
 
 ;; Undo (restore previous window configuration).
 (use-package winner ; Bundled with Emacs.
@@ -85,14 +73,8 @@
   (setq winner-dont-bind-my-keys t) ; Don't bind C-c <left> or C-c <right>.
   :config
   (winner-mode 1)
-  (bind-key "u" #'winner-undo my-window-map)
-  (bind-key "C-r" #'winner-redo my-window-map))
-
-;; Save/restore window configuration from register.
-(bind-key "m" #'window-configuration-to-register my-window-map)
-(bind-key "'" #'jump-to-register my-window-map)
-
-;; Find file in a new window (useful because it starts in the current window's directory).
-(bind-key "f" #'find-file-other-window my-window-map)
+  (with-eval-after-load 'evil
+    (bind-key "u" #'winner-undo evil-window-map)
+    (bind-key "C-r" #'winner-redo evil-window-map)))
 
 (provide 'conf/view/windows/window-bindings)
