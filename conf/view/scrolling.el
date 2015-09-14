41ce5b2f13dd83a37a46999a9a58b7eb57756c4b
Paweł Kraśnicki <dev@pkkm.eu>
2015-09-14 21:31:57 +0200

    Scroll by 10% instead of 5% of the window

--- conf/view/scrolling.el
+++ conf/view/scrolling.el
@@ -3,8 +3,8 @@
 ;; Keep 3 lines visible around the point.
 (setq-default scroll-margin 3)
 
-;; Scroll by 5% of the window.
-(setq-default scroll-up-aggressively 0.05
-              scroll-down-aggressively 0.05)
+;; Scroll by 10% of the window.
+(setq-default scroll-up-aggressively 0.1
+              scroll-down-aggressively 0.1)
 
 (provide 'conf/view/scrolling)
