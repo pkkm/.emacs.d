c590093ae829a653390abad58f1949879fad3b98
Paweł Kraśnicki <dev@pkkm.eu>
2015-09-15 19:35:31 +0200

    Revert "Remove newline inserting bindings (...)"
    This reverts commit c6f7fd0ec1edf81d1f2973e77a488fa2991c5125.

---
+++ conf/useless-without-evil/insert-newline.el
@@ -0,0 +1,20 @@
+;;; Insert newline after/before the current line.
+;; The wrappers are needed because the Evil functions aren't interactive.
+;; TODO feature request to Evil to make them interactive and accepting count.
+
+(with-eval-after-load 'evil
+  (require 'cl-lib) ; Used: cl-dotimes.
+
+  (defun my-insert-line-below (count)
+    (interactive "p")
+    (cl-dotimes (unused-var count)
+      (evil-insert-newline-below)))
+  (bind-key "g o" #'my-insert-line-below evil-normal-state-map)
+
+  (defun my-insert-line-above (count)
+    (interactive "p")
+    (cl-dotimes (unused-var count)
+      (evil-insert-newline-above)))
+  (bind-key "g O" #'my-insert-line-above evil-normal-state-map))
+
+(provide 'conf/useless-without-evil/insert-newline)
