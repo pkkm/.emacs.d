e084bd2711b32dc4c56c7883a7aa07148da1c394
Paweł Kraśnicki <dev@pkkm.eu>
2015-09-18 20:39:05 +0200

    Add flyspell configuration

---
+++ conf/driving-processes/spell-check.el
@@ -0,0 +1,17 @@
+;;; Spell checking.
+
+(use-package flyspell ; Included in Emacs.
+  :defer t
+  :diminish "FlyS" ; Default: "Fly".
+  :config
+
+  ;; Make a correct flyspell-mode hook (`flyspell-mode-hook' runs on both enable and disable; TODO bug report?).
+  (defun run-my-flyspell-mode-hook ()
+    (when flyspell-mode
+      (run-hooks 'my-flyspell-mode-hook)))
+  (add-hook 'flyspell-mode-hook #'run-my-flyspell-mode-hook)
+
+  ;; Check whole buffer after turning Flyspell on.
+  (add-hook 'my-flyspell-mode-hook #'flyspell-buffer))
+
+(provide 'conf/driving-processes/spell-check)
