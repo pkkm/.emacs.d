02ec0d5ddd08952aea29fb55ad72b3a5012b277e
Paweł Kraśnicki <dev@pkkm.eu>
2015-09-30 17:54:40 +0200

    ws-butler: don't automatically replace tabs with spaces

--- conf/opening-saving/trailing-whitespace.el
+++ conf/opening-saving/trailing-whitespace.el
@@ -6,7 +6,12 @@
   :ensure t
   :diminish ws-butler-mode
   :init
-  (add-hook 'prog-mode-hook #'ws-butler-mode))
+  (add-hook 'prog-mode-hook #'ws-butler-mode)
+  :config
+  ;; Don't replace tabs with spaces, even when `indent-tabs-mode' is nil.
+  (defadvice ws-butler-clean-region (around my-ws-butler-dont-untabify activate)
+    (let ((indent-tabs-mode t))
+      ad-do-it)))
 
 ;;; Highlight trailing whitespace in a buffer iff it's visiting a file.
 
