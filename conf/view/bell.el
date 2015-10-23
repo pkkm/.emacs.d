d42efb50fc8bd6291911f121db086e054f498892
Paweł Kraśnicki <dev@pkkm.eu>
2015-10-23 20:10:59 +0200

    Replace audible bell with echo area blinking

---
+++ conf/view/bell.el
@@ -0,0 +1,14 @@
+;;; Make the bell less annoying that either the audible or the visible one.
+
+(use-package echo-bell
+  :ensure t
+  :defer t
+  :init
+
+  ;; Variables have to be set before the package is loaded (as of 2015-10).
+  (setq echo-bell-string "")
+  (setq echo-bell-background (face-foreground 'success nil t)) ; TODO update when color theme changes.
+  (setq echo-bell-delay 0.05)
+  (echo-bell-mode))
+
+(provide 'conf/view/bell)
