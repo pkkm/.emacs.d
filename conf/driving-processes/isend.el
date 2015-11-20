151c5c32c2932a94e21e298a3058988221d996e8
Paweł Kraśnicki <dev@pkkm.eu>
2015-11-20 13:27:02 +0100

    Add command for creating isend-associated terminal
    Replace Scala-specific code for starting an isend-associated REPL, and
    code for starting a REPL in a `term' buffer, with a general command for
    creating an isend-associated `ansi-term'.

--- conf/driving-processes/isend.el
+++ conf/driving-processes/isend.el
@@ -3,6 +3,30 @@
 (use-package isend-mode
   :ensure t
   :defer t
+  :init
+
+  (defun isend-associated-ansi-term (program)
+    "Start PROGRAM in `ansi-term', in a window chosen by `display-buffer' and `isend-associate' the current buffer with it.
+The terminal will be in line mode: edit a line at a time and then send it with `term-send-input' (RET)."
+    (interactive (list (read-from-minibuffer "Run program: ")))
+    (let (term-buffer
+          ;; `ansi-term' normally displays the terminal in the current window, but we want it in another, so we'll create a temporary buffer, display it and launch `ansi-term' there.
+          (buffer-for-display (generate-new-buffer "*place-for-term*")))
+      (with-selected-window (display-buffer buffer-for-display)
+        (let ((term-ansi-buffer-base-name t)) ; Name the buffer after the program.
+          (ansi-term program)) ; `ansi-term' always creates a new buffer (in contrast to `term', which reuses).
+        (term-line-mode)
+        (evil-insert-state)
+        (kill-buffer buffer-for-display)
+        (setq term-buffer (current-buffer)))
+      (if (fboundp 'isend-associate)
+          (progn
+            (isend-associate (buffer-name term-buffer))
+            (message "Use M-RET (`isend-send') to send a line or region to the REPL."))
+        (message (concat "Use M-x isend-associate RET %s RET and then "
+                         "M-RET (`isend-send') to send lines/regions to the REPL.")
+                 (buffer-name term-buffer)))))
+
   :config
 
   ;; M-RET -- send the current line (or region) to the associated REPL.
--- conf/mode-specific/scala.el
+++ conf/mode-specific/scala.el
@@ -10,13 +10,6 @@
     (smart-tabs-add-language-support scala scala-mode-hook
       ((scala-indent:indent-line . scala-indent:step)))
     (smart-tabs-insinuate 'scala)
-    (add-hook 'scala-mode-hook #'enable-indent-tabs-mode))
-
-  (require 'conf/utils/repl) ; Used: repl-in-term.
-  (defun scala-repl ()
-    "Launch a Scala REPL in another window.
-If the current buffer is in Scala mode, `isend-associate' it with the REPL."
-    (interactive)
-    (repl-in-term "scala-repl" "scala" (eq major-mode 'scala-mode))))
+    (add-hook 'scala-mode-hook #'enable-indent-tabs-mode)))
 
 (provide 'conf/mode-specific/scala)
