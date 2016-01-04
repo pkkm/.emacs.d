1f4541032f9dae5c9e1bdbc45f9ee67a89418c29
Paweł Kraśnicki <dev@pkkm.eu>
2016-01-04 15:55:00 +0100

    Repeat command with g SPC instead of RET

--- conf/evil-specific/repeat-command.el
+++ conf/evil-specific/repeat-command.el
@@ -1,6 +1,6 @@
 ;;; Repeat command with RET.
 
 (with-eval-after-load 'evil
-  (bind-key "RET" #'repeat evil-motion-state-map))
+  (bind-key "g SPC" #'repeat evil-motion-state-map))
 
 (provide 'conf/evil-specific/repeat-command)
