6ba22240ca379d26363a07f771cd845a02d52e4f
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2016-05-04 13:23:39 +0200

    Remove code for starting Git credential cache
    Recent versions of Magit do this automatically.

--- conf/driving-processes/version-control.el
+++ conf/driving-processes/version-control.el
@@ -34,18 +34,7 @@ Then, restore the saved window configuration from before launching `magit-status
   ;; Start writing commit message in insert mode.
   (with-eval-after-load 'evil
     (evil-set-initial-state 'git-commit-mode 'insert) ; Pre-2.1.0.
-    (add-hook 'with-editor-mode-hook 'evil-insert-state)) ; 2.1.0+.
-
-  ;; When Magit is started, ensure credential cache daemon is running.
-  ;; (Necessary for password caching to work as of 2015-04. File Magit feature request?)
-  ;; TODO does this apply to 2.1.0+?
-  (add-hook 'magit-mode-hook 'my-magit-run-credential-cache-daemon)
-  (defun my-magit-run-credential-cache-daemon ()
-    (let ((socket-path (expand-file-name "~/.git-credential-cache/socket")))
-      (unless (file-exists-p socket-path)
-        (let ((process (start-process "credential-cache-daemon" nil "git"
-                                      "credential-cache--daemon" socket-path)))
-          (set-process-query-on-exit-flag process nil)))))) ; Kill without asking when exiting Emacs.
+    (add-hook 'with-editor-mode-hook 'evil-insert-state))) ; 2.1.0+.
 
 ;; Disable VC-mode (default Emacs interface for VCSes).
 (setq vc-handled-backends '())
