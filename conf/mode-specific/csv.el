f32587635dd52f967449f093e2751f31475a9326
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2016-06-26 15:45:39 +0200

    Use csv-mode for *.tsv files; fix error

--- conf/mode-specific/csv.el
+++ conf/mode-specific/csv.el
@@ -1,7 +1,16 @@
 ;;; CSV, TSV, etc.
 
-(use-package 'csv-mode
+(use-package csv-mode
   :ensure t
+  :init
+
+  ;; Use for *.tsv files.
+  ;; TODO: set `csv-separators' buffer-locally instead of globally.
+  (defun my-tsv-in-csv-mode ()
+    (csv-mode)
+    (custom-set-variables '(csv-separators '("\t")))) ; We can't use `setq' because the `:set' hook wouldn't be run.
+  (add-to-list 'auto-mode-alist '("\\.tsv\\'" . my-tsv-in-csv-mode))
+
   :config
 
   ;; Make the TAB key insert a literal TAB.
