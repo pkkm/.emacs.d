1462b9ef2860e9add7cd08767e5b67bdb2d9ca44
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2016-08-03 12:52:53 +0200

    Add support for YAML (including Ansible files)

---
+++ conf/mode-specific/yaml.el
@@ -0,0 +1,23 @@
+;;; YAML (including Ansible files).
+
+(use-package yaml-mode
+  :ensure t)
+
+(use-package ansible
+  :ensure t
+  :init
+  (defun ansible-mode-maybe-enable ()
+    (when (and (stringp (buffer-file-name))
+               (string-match "\\(site\.yml\\|roles/.+\.yml\\|group_vars/.+\\|host_vars/.+\\)"
+                             (buffer-file-name)))
+      (ansible 1)))
+  (with-eval-after-load 'yaml-mode
+    (add-hook 'yaml-mode-hook #'ansible-mode-maybe-enable)))
+
+(use-package ansible-doc
+  :ensure t
+  :init
+  (with-eval-after-load 'ansible
+    (add-hook 'ansible::hook #'ansible-doc-mode)))
+
+(provide 'conf/mode-specific/yaml)
