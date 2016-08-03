5267a135cd7df5b8a5d74ad3311311977855f4c0
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2016-08-03 10:31:32 +0200

    view/gui-specific: fix unmatched paren

--- conf/view/gui-specific.el
+++ conf/view/gui-specific.el
@@ -44,8 +44,7 @@
 
   (setq frame-title-format
         '("Emacs: %b " (:eval (when (buffer-modified-p) "+ "))
-          "[" default-directory "]"))
-  ;;(setq icon-title-format frame-title-format)
+          "[" default-directory "]")))
 
 
 (provide 'conf/view/gui-specific)
