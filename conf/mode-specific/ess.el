19cfe8bbd82af30d497e9f8e113e6a80278fa754
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2016-12-29 16:35:21 +0100

    mode-specific/ess: auto-install ESS

--- conf/mode-specific/ess.el
+++ conf/mode-specific/ess.el
@@ -1,6 +1,7 @@
 ;;; ESS (Emacs Speaks Statistics). -*- lexical-binding: t -*-
 
 (use-package ess
+  :ensure t
   :config
   (with-eval-after-load 'ess-s-l
     (ess-toggle-S-assign nil))) ; Don't replace "_" with " <- ".
