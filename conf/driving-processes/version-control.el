541a347876db11aaa2fa291f6da29d3bbf5188ba
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2017-03-21 14:58:31 +0100

    driving-processes/version-control: refactor

--- conf/driving-processes/version-control.el
+++ conf/driving-processes/version-control.el
@@ -19,22 +19,7 @@
   :config
 
   ;; Make `magit-status' take up the whole screen. When exiting it, restore the closed windows.
-  (defadvice magit-status (around my-magit-fullscreen activate)
-    "Save window configuration and make `magit-status' take up the whole screen."
-    (window-configuration-to-register :my-magit-fullscreen)
-    ad-do-it
-    (delete-other-windows))
-  (defun my-magit-fullscreen-quit ()
-    "Do what pressing `q' would normally do in `magit-status'.
-Then, restore the saved window configuration from before launching `magit-status'."
-    (interactive)
-    (when my-magit-fulscreen-previous-q-command
-      (call-interactively my-magit-fulscreen-previous-q-command))
-    (jump-to-register :my-magit-fullscreen))
-  (defvar my-magit-fulscreen-previous-q-command
-    (lookup-key magit-status-mode-map (kbd "q") t)
-    "The command that was bound to `q' in `magit-status' before `q' was rebound.")
-  (bind-key "q" #'my-magit-fullscreen-quit magit-status-mode-map)
+  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
 
   ;; Start writing commit message in insert mode.
   (with-eval-after-load 'evil
