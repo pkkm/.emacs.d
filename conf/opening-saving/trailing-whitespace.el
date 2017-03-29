edca578696467131145143d8cc727d6846ebab8b
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2017-03-29 11:31:57 +0200

    opening-saving/trailing-whitespace: don't override file-local show-trailing-whitespace

--- conf/opening-saving/trailing-whitespace.el
+++ conf/opening-saving/trailing-whitespace.el
@@ -12,14 +12,15 @@
 
 (setq-default show-trailing-whitespace nil)
 
-(defun show-trailing-whitespace-iff-visiting-file ()
-  "Display trailing whitespace in the current buffer iff it's visiting a file.
+(defun my-set-show-trailing-whitespace ()
+  "Show trailing whitespace iff visiting a file (unless overridden via a file local variable).
 Returns nil so that it can be added to hooks which terminate when a function returns t, e.g. `find-file-functions'."
-  (setq show-trailing-whitespace (not (not buffer-file-name)))
+  (unless (assq 'show-trailing-whitespace file-local-variables-alist)
+    (setq show-trailing-whitespace (not (not buffer-file-name))))
   nil)
 
 (require 'conf/utils/hooks) ; Used: add-hooks.
 (add-hooks '(find-file-hook write-file-functions)
-           #'show-trailing-whitespace-iff-visiting-file)
+           #'my-set-show-trailing-whitespace)
 
 (provide 'conf/opening-saving/trailing-whitespace)
