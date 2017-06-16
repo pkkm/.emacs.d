73307b80f5f51ff45c2e72867b0d0bfbaaa8412a
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2017-06-16 13:19:20 +0200

    Remove minibuffer/lacarte (unused)

--- conf/minibuffer/lacarte.el
+++
@@ -1,7 +0,0 @@
-;;; Lacarte -- execute menu commands using the keyboard. -*- lexical-binding: t -*-
-
-(use-package lacarte
-  :ensure t
-  :bind ("C-x m" . lacarte-execute-menu-command))
-
-(provide 'conf/minibuffer/lacarte)
