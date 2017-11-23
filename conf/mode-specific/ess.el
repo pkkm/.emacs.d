75c5bb58d4c1c2daf886c668856d9fba1ea7d8a3
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2017-11-23 09:13:40 +0100

    mode-specific/ess: fix disabling of "smart assign"

--- conf/mode-specific/ess.el
+++ conf/mode-specific/ess.el
@@ -4,6 +4,6 @@
   :ensure t
   :config
   (with-eval-after-load 'ess-s-l
-    (ess-toggle-S-assign nil))) ; Don't replace "_" with " <- ".
+    (ess-disable-smart-S-assign nil))) ; Don't replace "_" with " <- ".
 
 (provide 'conf/mode-specific/ess)
