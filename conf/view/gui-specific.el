6b46e342d49b0c5f796c099347dbbe27e91e322e
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2017-12-02 14:19:36 +0100

    view/gui-specific: increase font size; refactor (minor)

--- conf/view/gui-specific.el
+++ conf/view/gui-specific.el
@@ -2,23 +2,30 @@
 
 (when (display-graphic-p)
 
-
-  ;;; Cursor (point).
+  ;; Cursor (point).
   ;; (Cursor color is set in view/color-theme.el.)
   (blink-cursor-mode 0)
   (setq x-stretch-cursor t) ; Stretch the cursor to cover wide characters (e.g. tabs).
+
+  ;; Resize frames by pixels instead of characters.
+  (setq frame-resize-pixelwise t)
+
+  ;; Focus follows mouse.
+  (setq mouse-autoselect-window t)
+
+  ;; Frame title.
+  (setq frame-title-format
+        '("Emacs: %b " (:eval (when (buffer-modified-p) "+ "))
+          "[" default-directory "]"))
+  (setq icon-title-format frame-title-format)) ; Don't change the title when minimized.
 
 
   ;;; Fringe.
   (set-fringe-mode '(0 . 0)) ; Disable both left and right fringes.
 
-  ;; Invisible (same color as background).
+  ;; Same color as the background.
   ;;(face-spec-reset-face 'fringe)
   ;;(set-face-attribute 'fringe nil :inherit 'default)
-
-
-  ;;; Focus follows mouse.
-  (setq mouse-autoselect-window t)
 
 
   ;;; Font.
@@ -32,20 +39,8 @@
     (-if-let (font (first-available-font fonts))
         (set-face-font face font)))
 
-  (set-first-available-font 'default '("DejaVu Sans Mono-9" "Consolas-10" "Courier New-9.5"))
+  (set-first-available-font 'default '("DejaVu Sans Mono-9.5" "Consolas-10" "Courier New-9.5"))
   (set-first-available-font 'variable-pitch '("DejaVu Sans-9.6" "Verdana" "Helvetica" "Arial")) ; Causes segfault in `emacs -nw'.
-
-
-  ;;; Resize frames by pixels instead of characters.
-  (setq frame-resize-pixelwise t)
-
-
-  ;;; Frame title.
-
-  (setq frame-title-format
-        '("Emacs: %b " (:eval (when (buffer-modified-p) "+ "))
-          "[" default-directory "]"))
-  (setq icon-title-format frame-title-format)) ; Don't change the title when minimized.
 
 
 (provide 'conf/view/gui-specific)
