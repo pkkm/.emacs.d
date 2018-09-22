ce6037dc472b0f85c276a572ef4642be4404f644
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2018-09-22 18:03:46 +0200

    mode-specific/calc: refactor (minor)

--- conf/mode-specific/calc.el
+++ conf/mode-specific/calc.el
@@ -1,16 +1,10 @@
 ;;; Customization of Emacs calculator mode. -*- lexical-binding: t -*-
 
-(defun my-calc-workspace ()
-  "Display only Calc and Trail buffers."
-  (interactive)
-  (calc)
-  (delete-other-windows))
-
 ;; Use the `--calc' argument to start Emacs Calc-only.
-(defun my-calc-workspace-after-startup (&rest ignored-args)
-  "Call `my-calc-workspace' after Emacs startup."
-  (add-hook 'emacs-startup-hook #'my-calc-workspace t))
-(add-to-list 'command-switch-alist (cons "--calc" #'my-calc-workspace-after-startup))
+(defun my-calc-after-startup (&rest ignored-args)
+  "Open `calc' in fullscreen after Emacs startup."
+  (add-hook 'emacs-startup-hook #'full-calc t))
+(add-to-list 'command-switch-alist (cons "--calc" #'my-calc-after-startup))
 
 (use-package calc
   :config
