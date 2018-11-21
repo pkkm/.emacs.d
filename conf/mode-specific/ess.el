0b4d24eeeeeff72818ce29d05b94fccb2743fc8a
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2018-11-21 14:03:55 +0100

    mode-specific/ess: update disabling of "smart underscore"

--- conf/mode-specific/ess.el
+++ conf/mode-specific/ess.el
@@ -3,7 +3,6 @@
 (use-package ess
   :ensure t
   :config
-  (with-eval-after-load 'ess-s-l
-    (ess-disable-smart-S-assign nil))) ; Don't replace "_" with " <- ".
+  (setq ess-smart-S-assign-key nil)) ; Don't replace "_" with " <- ".
 
 (provide 'conf/mode-specific/ess)
