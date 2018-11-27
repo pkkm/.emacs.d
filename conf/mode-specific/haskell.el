fc57d19caa3c92dc714bf6af71f2ef9d2c3e667f
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2018-11-27 19:55:58 +0100

    mode-specific/haskell: add Intero

--- conf/mode-specific/haskell.el
+++ conf/mode-specific/haskell.el
@@ -1,15 +1,15 @@
 ;;; Haskell. -*- lexical-binding: t -*-
-;; For IDE features, install Intero or haskell-ide-engine. (As of 2018-11, Intero is more mature but haskell-ide-engine is more advanced.)
 
 ;; Main package.
 (use-package haskell-mode
   :ensure t
   :config
 
-  (add-hook 'haskell-mode-hook #'interactive-haskell-mode) ; Use the new interactive mode.
-  (add-hook 'haskell-mode-hook #'eldoc-mode) ; Show type signatures in minibuffer (eldoc config is in conf/view/eldoc.el). Uses interactive process when available, otherwise hardcoded list.
+  ;; Commented out since I'm using Intero instead.
+  ;; (add-hook 'haskell-mode-hook #'interactive-haskell-mode)
+  (add-hook 'haskell-mode-hook #'eldoc-mode) ; Show type signatures in minibuffer. Uses interactive process when available, hardcoded list otherwise.
 
-  ;; Customizations suggested on <https://github.com/haskell/haskell-mode/wiki/Haskell-Interactive-Mode-Setup>.
+  ;; Customizations for haskell-interactive-mode.
   (setq haskell-process-suggest-remove-import-lines t)
   (setq haskell-process-auto-import-loaded-modules t)
   (setq haskell-process-log t)
@@ -17,13 +17,18 @@
   ;; Use Emacs state for error buffers so that they can be quickly closed with "q".
   (evil-set-initial-state 'haskell-error-mode 'emacs))
 
-;; Auto-configure Flycheck.
-(use-package flycheck-haskell
+;; IDE features. (As of 2018-11, this package is more mature but less advanced than haskell-ide-engine. Consider switching in the second half of 2019.)
+(use-package intero
   :ensure t
   :init
-  (with-eval-after-load 'haskell-mode ; No need to load if we're not editing any Haskell file.
-    (with-eval-after-load 'flycheck
-      (add-hook 'flycheck-mode-hook #'flycheck-haskell-setup))))
+  (add-hook 'haskell-mode-hook #'intero-mode))
+
+;; Auto-configure Flycheck (unnecessary with Intero).
+;; (use-package flycheck-haskell
+;;   :ensure t
+;;   :init
+;;   (with-eval-after-load 'haskell-mode ; No need to load if we're not editing any Haskell file.
+;;     (add-hook 'flycheck-mode-hook #'flycheck-haskell-setup)))
 
 ;; Additional snippets.
 (use-package haskell-snippets
