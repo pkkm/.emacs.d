d69dd593c47075dd9dfe2db0ad93c4d4d70d4c78
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2018-11-30 10:04:08 +0100

    mode-specific/ess: add comments about keybindings

--- conf/mode-specific/ess.el
+++ conf/mode-specific/ess.el
@@ -1,5 +1,14 @@
 ;;; ESS (Emacs Speaks Statistics). -*- lexical-binding: t -*-
 
+;; Useful keybindings:
+;;   C-c C-z -- switch to REPL / back to text file.
+;;   C-c C-s -- change REPL process associated with buffer.
+;;   C-c C-j -- evaluate line.
+;;   C-c C-c -- evaluate region, function or paragraph.
+;;   C-c C-b -- evaluate buffer.
+;;   C-c C-v -- open help.
+;;   , (in REPL) -- open a menu with some handy commands, e.g. installing a package.
+
 (use-package ess
   :ensure t
   :config
