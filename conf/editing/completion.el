57849b29d87abab4e7ffbacc9d04a177f20d19d6
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2019-03-03 00:51:48 +0100

    editing/completion: remove auto-complete

--- conf/editing/completion.el
+++ conf/editing/completion.el
@@ -5,107 +5,6 @@
 
 (add-to-list 'completion-styles 'initials t) ; E.g. ~/sew -> ~/src/emacs/work.
 
-;; Old completion package -- currently unused but kept in case I need it later.
-(use-package auto-complete
-  :ensure t
-  :diminish auto-complete-mode
-  :init
-
-  (defvar my-major-mode-ac-sources '()
-    "Additional `auto-complete' sources to use in various major-modes.
-Format: '((major-mode . (ac-source ...)) ...)")
-  ;; This has to be here because it's used by other parts of my config.
-
-  :config
-
-  ;; Make Company and auto-complete mutually exclusive.
-  (with-eval-after-load 'company
-    (defun my-disable-ac-when-company-active ()
-      (when (and (bound-and-true-p auto-complete-mode)
-                 (bound-and-true-p company-mode))
-        (auto-complete-mode -1)))
-    (defun my-disable-company-when-ac-active ()
-      (when (and (bound-and-true-p auto-complete-mode)
-                 (bound-and-true-p company-mode))
-        (company-mode -1)))
-    (add-hook 'company-mode-hook #'my-disable-ac-when-company-active)
-    (add-hook 'auto-complete-mode-hook #'my-disable-company-when-ac-active))
-
-  ;; Don't disable auto-complete on specific faces.
-  (setq ac-disable-faces nil)
-
-  ;; Automatic activation.
-  (setq ac-delay 0.05)
-  (setq ac-auto-show-menu t) ; Show menu immediately (skip showing a candidate inline).
-
-  ;; Performance (change if Emacs is too slow).
-  (setq ac-auto-start 2) ; Start completion after 2 letters.
-  (setq ac-candidate-limit 1000) ; Upper limit until Emacs becomes annoyingly sluggish (tested on Box in 2017-09).
-
-  ;; Help.
-  (setq ac-quick-help-delay 1)
-
-  ;; Use auto-complete for `completion-at-point'.
-  ;; `completion-at-point' is used when TAB is pressed, the current line is already properly indented and:
-  ;;   * `tab-always-indent' is set to 'complete
-  ;;   * auto-complete isn't already being displayed (when it is, the TAB binding in `ac-completing-map' is used instead)
-  (require 'conf/utils/lists) ; Used: add-to-list-after.
-  (defun auto-complete-if-active ()
-    (when auto-complete-mode #'auto-complete))
-  (defun add-ac-to-completion-at-point ()
-    (add-to-list-after 'completion-at-point-functions
-                       #'auto-complete-if-active
-                       #'yas-expand-if-active))
-  (add-hook 'auto-complete-mode-hook #'add-ac-to-completion-at-point)
-
-
-  ;;; Bindings.
-
-  ;; Delete default bindings.
-  (require 'conf/utils/keys) ; Used: clear-keymap.
-  (clear-keymap ac-completing-map) ; There is also `ac-menu-map', for when the menu is active.
-
-  ;; Cycle candidates with arrow keys.
-  (bind-key "<down>" #'ac-next ac-completing-map)
-  (bind-key "<up>" #'ac-previous ac-completing-map)
-
-  ;; Completing.
-  (bind-key "TAB" #'ac-expand ac-completing-map) ; Expand; cycle candidates when pressed repeatedly.
-  (bind-key "<backtab>" #'ac-previous ac-completing-map)
-  (bind-key "RET" #'ac-complete ac-completing-map) ; Choose the current candidate and perform its associated action.
-
-  ;; Help.
-  (bind-key "M-p" #'ac-quick-help-scroll-up ac-completing-map)
-  (bind-key "M-n" #'ac-quick-help-scroll-down ac-completing-map)
-  (bind-key "<f1>" #'ac-persist-help ac-completing-map) ; Open help in a buffer.
-
-
-  ;;; Sources.
-
-  (require 'auto-complete-config) ; Used: ac-source-yasnippet.
-  (setq-default ac-sources '(ac-source-words-in-buffer
-                             ac-source-yasnippet
-                             ac-source-dictionary
-                             ac-source-words-in-same-mode-buffers
-                             ac-source-words-in-all-buffer
-                             ac-source-filename))
-
-  ;; Automatically add sources from `my-major-mode-ac-sources'.
-  (require 'conf/utils/modes) ; Used: derived-mode-hierarchy.
-  (require 'cl-lib) ; Used: cl-remove-duplicates.
-  (defun my-set-ac-sources ()
-    "Add auto-complete sources from `my-ac-major-mode-sources' for the given major mode (and its parent modes)."
-    (setq ac-sources
-          (--> (derived-mode-hierarchy major-mode)
-            (-map (lambda (mode)
-                    (cdr (assoc mode my-major-mode-ac-sources)))
-                  it)
-            (apply #'append it)
-            (append it ac-sources)
-            (cl-remove-duplicates it :from-end t)))) ; If a source occurs more than once, retain the earliest occurrence.
-  (add-hook 'after-change-major-mode-hook #'my-set-ac-sources))
-
-
 (use-package company
   :ensure t
   :init
@@ -152,7 +51,7 @@ Format: '((major-mode . (ac-source ...)) ...)")
                        #'yas-expand-if-active))
   (add-hook 'company-mode-hook #'add-company-to-completion-at-point))
 
-;; Documentation popups for Company.
+;; Documentation popups.
 (use-package company-quickhelp
   :ensure t
   :init
