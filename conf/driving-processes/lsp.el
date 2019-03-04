5a3bbdc5bbf221946e68333782efe7ca3b04e13a
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2019-03-04 11:35:11 +0100

    driving-processes/lsp: simplify

--- conf/driving-processes/lsp.el
+++ conf/driving-processes/lsp.el
@@ -3,17 +3,13 @@
 (use-package lsp-mode
   :ensure t
   :config
-
   (require 'lsp-imenu)
-  (add-hook 'lsp-after-open-hook #'lsp-enable-imenu)
-
-  (add-hook 'lsp-after-open-hook #'company-mode))
+  (add-hook 'lsp-after-open-hook #'lsp-enable-imenu))
 
 (use-package lsp-ui
   :ensure t
   :init
-  (with-eval-after-load 'lsp-mode
-    (add-hook 'lsp-mode-hook #'lsp-ui-mode)))
+  (add-hook 'lsp-mode-hook #'lsp-ui-mode))
 
 (use-package company-lsp
   :ensure t
@@ -26,10 +22,7 @@
 ;; As of 2018-11, the Emacs package for cquery is more polished than ccls. (I tried both and ccls often failed to work without any indication of what's wrong.)
 (use-package cquery
   :ensure t
-
-  :init
-  (autoload 'lsp-cquery-enable "cquery" nil t)
-
+  :commands lsp-cquery-enable
   :config
   ;; Store cache in one place rather than polluting project directories.
   (setq cquery-cache-dir-function #'cquery-cache-dir-consolidated))
