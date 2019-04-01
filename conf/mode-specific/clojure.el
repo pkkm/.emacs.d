eb4ab38da23a32e70c83f386481b76b548c4248b
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2019-04-01 10:36:07 +0200

    mode-specific/clojure: update for new CIDER version

--- conf/mode-specific/clojure.el
+++ conf/mode-specific/clojure.el
@@ -9,31 +9,21 @@
   ;; (Configured in conf/view/eldoc.el.)
   (add-hook 'cider-mode-hook #'eldoc-mode))
 
-(use-package clojure-mode ; Installed by CIDER.
+(use-package clojure-mode
+  :ensure t
   :config
 
   ;; Indent guides.
   (require 'conf/view/indent-guides)
-  (add-hook 'clojure-mode-hook #'indent-guide-mode))
+  (add-hook 'clojure-mode-hook #'indent-guide-mode)
+
+  ;; Clojure cheatsheet.
+  (with-eval-after-load 'cider
+    (bind-key "C-c M-h" #'cider-cheatsheet clojure-mode-map)))
 
 ;; YASnippet snippets.
 (use-package clojure-snippets
   :ensure t)
-
-;; Clojure cheatsheet (C-c M-h).
-;; See also: CIDER's "C-c C-d g" binding to look up symbol in Grimoire (online Clojure reference).
-(use-package clojure-cheatsheet
-  :ensure t
-  :init
-  (with-eval-after-load 'clojure-mode
-    (defun clojure-cheatsheet-or-error ()
-      "Start `clojure-cheatsheet' if there's a nREPL connection (which it needs). Otherwise, error out."
-      (interactive)
-      (if (and (functionp 'nrepl-current-connection-buffer) ; Future-proofing in case the function name changes.
-               (not (nrepl-current-connection-buffer t)))
-          (error "An nREPL connection is needed!")
-        (call-interactively #'clojure-cheatsheet)))
-    (bind-key "C-c M-h" #'clojure-cheatsheet-or-error clojure-mode-map)))
 
 ;; Refactoring.
 ;; Prefix: C-c RET (overrides the binding for macroexpand).
