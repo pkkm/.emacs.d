c2c9fb1c14f3d947a6301f730a451e503686827f
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2019-05-11 18:04:11 +0200

    Add view/shrink-blank-lines

---
+++ conf/view/shrink-blank-lines.el
@@ -0,0 +1,18 @@
+;;; Minor mode that decreases the height of blank lines. -*- lexical-binding: t -*-
+
+(defface shrink-blank-lines-face
+  '((t :height 0.6))
+  "Face applied to blank lines.")
+
+(defconst shrink-blank-lines--keywords
+  '(("^[ \t]*\n" 0 'shrink-blank-lines-face prepend)))
+
+(define-minor-mode shrink-blank-lines-mode
+  "Shrink blank lines."
+  :lighter " ShrinkB"
+  (if shrink-blank-lines-mode
+      (font-lock-add-keywords nil shrink-blank-lines--keywords 'append)
+    (font-lock-remove-keywords nil shrink-blank-lines--keywords))
+  (font-lock-flush))
+
+(provide 'conf/view/shrink-blank-lines)
