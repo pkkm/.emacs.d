4979fea5b703b3d47152a068313047717bb978aa
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2019-07-19 17:33:13 +0200

    driving-processes/lsp: fix cquery

--- conf/driving-processes/lsp.el
+++ conf/driving-processes/lsp.el
@@ -18,11 +18,17 @@
     (with-eval-after-load 'lsp-mode
       (add-to-list 'company-backends #'company-lsp))))
 
-;; As of 2018-11, the Emacs package for cquery is more polished than ccls. (I tried both and ccls often failed to work without any indication of what's wrong.)
+;; As of 2018-11, the Emacs package for cquery is more polished than ccls. (I tried both and ccls failed to work without any indication of what's wrong.)
 (use-package cquery
   :ensure t
-  :commands lsp-cquery-enable
+  :init
+
+  ;; Needed for M-x lsp to work (as of 2019-07).
+  (with-eval-after-load 'lsp-mode
+    (require 'cquery))
+
   :config
+
   ;; Store cache in one place instead of polluting project directories.
   (setq cquery-cache-dir-function #'cquery-cache-dir-consolidated))
 
