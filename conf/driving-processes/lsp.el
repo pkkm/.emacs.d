7db03977d24966d42b666377f26cbb63b6883503
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2019-07-19 17:31:14 +0200

    driving-processes/lsp: remove obsolete imenu code

--- conf/driving-processes/lsp.el
+++ conf/driving-processes/lsp.el
@@ -1,10 +1,8 @@
 ;;; Support for the Language Server Protocol. -*- lexical-binding: t -*-
+;; To enable, invoke M-x lsp (the server will be selected automatically).
 
 (use-package lsp-mode
-  :ensure t
-  :config
-  (require 'lsp-imenu nil t) ; For compatibility with lsp-mode versions before 2018-11-21.
-  (add-hook 'lsp-after-open-hook #'lsp-enable-imenu))
+  :ensure t)
 
 (use-package lsp-ui
   :ensure t
@@ -18,7 +16,6 @@
     (with-eval-after-load 'lsp-mode
       (add-to-list 'company-backends #'company-lsp))))
 
-;; To enable, invoke M-x lsp-cquery-enable. Don't use M-x lsp-mode -- lsp-cquery-enable will turn it on for you.
 ;; As of 2018-11, the Emacs package for cquery is more polished than ccls. (I tried both and ccls often failed to work without any indication of what's wrong.)
 (use-package cquery
   :ensure t
