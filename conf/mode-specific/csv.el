f4ee51c4076c1932115690e60316b9dd89712304
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2019-10-13 22:39:25 +0200

    mode-specific/csv: remove settings obsolete in new package version

--- conf/mode-specific/csv.el
+++ conf/mode-specific/csv.el
@@ -1,19 +1,6 @@
-;;; CSV, TSV, etc. -*- lexical-binding: t -*-
+;;; CSV and TSV. -*- lexical-binding: t -*-
 
 (use-package csv-mode
-  :ensure t
-  :init
-
-  ;; Use for *.tsv files.
-  ;; TODO: set `csv-separators' buffer-locally instead of globally.
-  (defun my-tsv-in-csv-mode ()
-    (csv-mode)
-    (custom-set-variables '(csv-separators '("\t")))) ; We can't use `setq' because the `:set' hook wouldn't be run.
-  (add-to-list 'auto-mode-alist '("\\.tsv\\'" . my-tsv-in-csv-mode))
-
-  :config
-
-  ;; Make the TAB key insert a literal TAB.
-  (bind-key "TAB" #'self-insert-command csv-mode-map))
+  :ensure t)
 
 (provide 'conf/mode-specific/csv)
