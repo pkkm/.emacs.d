a5ef3d7ef9542e0641719aa3f03e78def548ab81
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2022-08-17 15:14:35 +0200

    opeting-saving/auto-revert: use file notifications when available

--- conf/opening-saving/auto-revert.el
+++ conf/opening-saving/auto-revert.el
@@ -2,4 +2,8 @@
 
 (global-auto-revert-mode 1)
 
+;; Save power by using inotify or an equivalent instead of polling every `auto-revert-interval' seconds.
+(when (version<= "27.1" emacs-version)
+  (setq auto-revert-avoid-polling t))
+
 (provide 'conf/opening-saving/auto-revert)
