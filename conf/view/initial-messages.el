304b8ea412a3efe9793eca2137479f0525a3b864
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2022-08-26 01:49:23 +0200

    view/initial-messages: manually format the init time
    The format returned by the `emacs-init-time` function has changed between Emacs
    version, so it's better do do it ourselves.

--- conf/view/initial-messages.el
+++ conf/view/initial-messages.el
@@ -14,7 +14,9 @@
 
 ;; Show init time in the minibuffer instead of the default message.
 (defun startup-echo-area-message ()
-  (format "Init time: %s." (emacs-init-time)))
+  (format "Init time: %.2f seconds."
+          ;; Similar to `emacs-init-time'.
+          (float-time (time-subtract after-init-time before-init-time))))
 
 ;; Don't display the startup screen.
 (setq inhibit-startup-message t)
