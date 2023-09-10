bcd260816dc7012eb85814938b0cc5a2a39bae9e
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2023-09-10 16:04:55 +0200

    opening-saving/editorconfig: update for current version of package

--- conf/opening-saving/editorconfig.el
+++ conf/opening-saving/editorconfig.el
@@ -1,6 +1,9 @@
 ;;; Support for the EditorConfig format for defining coding styles (see editorconfig.org). -*- lexical-binding: t -*-
 
 (use-package editorconfig
-  :ensure t) ; The package is initialized automatically. (The `edconf-find-file-hook' function is added to `find-file-hook'.)
+  :ensure t
+  :diminish editorconfig-mode
+  :init
+  (editorconfig-mode 1))
 
 (provide 'conf/opening-saving/editorconfig)
