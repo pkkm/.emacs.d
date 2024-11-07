774210cd314a8ffea6ec0e5ed3ef8b5fcb9fe2cb
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2024-11-07 15:37:26 +0100

    early-init.el: disable native compilation

--- early-init.el
+++ early-init.el
@@ -1,4 +1,8 @@
 ;;; Code executed before the package system is initialized. -*- lexical-binding: t -*-
 
+;; Disable native compilation (I'd rather wait until it's better tested by people).
+(setq native-comp-deferred-compilation nil)
+(setq native-comp-speed -1)
+
 ;; Don't automatically call `package-initialize' before (Emacs 27+) or after (Emacs <27) `init.el'.
 (setq package-enable-at-startup nil)
