5c9b8cd0cc8de3fdde8fef149264579832db89bb
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2024-12-13 19:09:45 +0100

    mode-specific/puppet: fix smart tabs indentation

--- conf/mode-specific/puppet.el
+++ conf/mode-specific/puppet.el
@@ -9,6 +9,9 @@
     (smart-tabs-add-language-support puppet puppet-mode-hook
       ((puppet-indent-line . puppet-indent-level)))
     (smart-tabs-insinuate 'puppet)
-    (add-hook 'puppet-mode-hook #'enable-indent-tabs-mode)))
+    (add-hook 'puppet-mode-hook #'enable-indent-tabs-mode)
+
+    ;; This used not to be necessary before I updated to Emacs 28 and newer packages. I don't know why it's necessary now. It seems that other people don't either - see <https://github.com/jcsalomon/smarttabs/issues/51>.
+    (setq puppet-indent-level tab-width)))
 
 (provide 'conf/mode-specific/puppet)
