8d95a697e9a4f1b31ef613cb890cda2cbd370c66
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-05 20:48:01 +0100

    mode-specific/python: switch from Elpy to Pyright LSP

--- conf/mode-specific/python.el
+++ conf/mode-specific/python.el
@@ -9,21 +9,14 @@
   ;; Don't add an extra empty line when filling docstrings.
   (setq python-fill-docstring-style 'pep-257-nn))
 
-(use-package elpy
+;; I've switched from Elpy to LSP-only, as Elpy is not actively maintained anymore (as of 2025).
+
+(use-package lsp-pyright
   :ensure t
   :init
-  (with-eval-after-load 'python
-    (elpy-enable))
-  :config
 
-  ;; Don't change global modeline settings.
-  (setq elpy-remove-modeline-lighter nil)
-
-  (setq elpy-rpc-python-command "python3")
-  (setq elpy-rpc-backend "jedi") ; Better completion than Rope (as of 2015-05).
-
-  ;; Disable some modules.
-  (let ((disabled-modules '(elpy-module-flymake elpy-module-highlight-indentation)))
-    (setq elpy-modules (-difference elpy-modules disabled-modules))))
+  ;; Use basedpyright (a slighly improved Pyright) when available.
+  (when (executable-find "basedpyright")
+    (setq lsp-pyright-langserver-command "basedpyright")))
 
 (provide 'conf/mode-specific/python)
