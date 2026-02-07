b17506503e455057c8d83ee8d10c9958fc40958c
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-08 00:25:38 +0100

    minibuffer/M-x: replace smex with amx

--- conf/minibuffer/M-x.el
+++ conf/minibuffer/M-x.el
@@ -1,10 +1,10 @@
-;;; Smex -- like Ido mode, for M-x. -*- lexical-binding: t -*-
+;;; Amx -- like Ido mode, for M-x. -*- lexical-binding: t -*-
 
-(use-package smex
+(use-package amx
   :ensure t
-  :bind (("M-x" . smex)
-         ("C-x SPC" . smex)))
+  :bind (("M-x" . amx)
+         ("C-x SPC" . amx)))
 
-;; Interesting command: smex-show-unbound-commands -- show frequently called commands that are unbound.
+;; Interesting command: amx-show-unbound-commands -- show frequently called commands that are unbound.
 
 (provide 'conf/minibuffer/M-x)
