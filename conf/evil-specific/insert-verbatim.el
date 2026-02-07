941bfdb304a05e6d20325a0b0ec9749e16cf7184
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-07 16:49:00 +0100

    Remove unnecessary evil-specific/insert-verbatim.el

--- conf/evil-specific/insert-verbatim.el
+++
@@ -1,7 +0,0 @@
-;;; Insert a character verbatim. -*- lexical-binding: t -*-
-
-(with-eval-after-load 'evil
-  (bind-key "C-v" #'quoted-insert evil-insert-state-map)
-  (bind-key "C-v" #'quoted-insert evil-replace-state-map))
-
-(provide 'conf/evil-specific/insert-verbatim)
