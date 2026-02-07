aa63638bc2edd3b3d864228c40ed2747a219ae2b
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-07 16:49:17 +0100

    evil-specific/insert-newline: don't use cl-lib unnecessarily

--- conf/evil-specific/insert-newline.el
+++ conf/evil-specific/insert-newline.el
@@ -3,17 +3,15 @@
 ;; TODO feature request to Evil to make them interactive and accepting count.
 
 (with-eval-after-load 'evil
-  (require 'cl-lib) ; Used: cl-dotimes.
-
   (defun my-insert-line-below (count)
     (interactive "p")
-    (cl-dotimes (_ count)
+    (dotimes (_ count)
       (evil-insert-newline-below)))
   (bind-key "g o" #'my-insert-line-below evil-normal-state-map)
 
   (defun my-insert-line-above (count)
     (interactive "p")
-    (cl-dotimes (_ count)
+    (dotimes (_ count)
       (evil-insert-newline-above)))
   (bind-key "g O" #'my-insert-line-above evil-normal-state-map))
 
