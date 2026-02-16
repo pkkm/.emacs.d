305d4de11ce67806a3ed1d36dfcf9bc9b7f55928
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-16 22:15:07 +0100

    early-init.el: use new name of native compilation variable

--- early-init.el
+++ early-init.el
@@ -1,7 +1,7 @@
 ;;; Code executed before the package system is initialized. -*- lexical-binding: t -*-
 
 ;; Disable native compilation (I'd rather wait until it's better tested by people).
-(setq native-comp-deferred-compilation nil)
+(setq native-comp-jit-compilation nil)
 (setq native-comp-speed -1)
 
 ;; Don't automatically call `package-initialize' before `init.el'.
