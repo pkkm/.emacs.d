c7d5586dc00623713f56ea8c8b3643b7b350bbee
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-16 22:29:53 +0100

    opening-saving/backups: keep more versions

--- conf/opening-saving/backups.el
+++ conf/opening-saving/backups.el
@@ -1,12 +1,18 @@
 ;;; File backups. -*- lexical-binding: t -*-
 ;; To disable, (setq make-backup-files nil).
 
+;; TODO: Emacs feature request to add a variable specifying the maximum file size that should be backed up.
+
 (setq backup-by-copying t) ; Slower, but won't make hard links point to the backup.
 
 ;; Backup directory.
 (let ((backup-dir (locate-user-emacs-file "backups")))
   (setq backup-directory-alist `(("." . ,backup-dir))))
 
-(setq delete-old-versions t) ; Delete old backups silently.
+;; Keep more versions (default: 2).
+(setq kept-new-versions 4)
+
+;; Delete old backups silently.
+(setq delete-old-versions t)
 
 (provide 'conf/opening-saving/backups)
