3902c05449106c40d4190978f1c2732a2cfc9ef7
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-16 22:28:15 +0100

    opening-saving/encoding: remove code that's unnecessary in Emacs 30

--- conf/opening-saving/encoding.el
+++ conf/opening-saving/encoding.el
@@ -6,15 +6,6 @@
 ;;   C-x RET c
 
 ;; Coding system for newly created buffers, I/O, etc.
-(prefer-coding-system 'utf-8-unix)
 (set-default-coding-systems 'utf-8-unix)
-
-;; This probably should be left for Emacs to detect.
-;;(set-terminal-coding-system 'utf-8)
-;;(set-keyboard-coding-system 'utf-8)
-;;(set-language-environment 'utf-8)
-
-;; This almost certainly should be left for Emacs to detect.
-;;(setq locale-coding-system 'utf-8)
 
 (provide 'conf/opening-saving/encoding)
