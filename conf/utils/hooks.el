d90240c9b41ab96b17a858656fed4ffa95881450
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-16 22:48:42 +0100

    utils/hooks: modernize code

--- conf/utils/hooks.el
+++ conf/utils/hooks.el
@@ -2,30 +2,16 @@
 
 (defun add-hooks (hooks function)
   "Like `add-hook', but accepts multiple hooks (as a list of symbols)."
-  (mapc (lambda (hook)
-          (add-hook hook function))
-        hooks))
+  (dolist (hook hooks)
+    (add-hook hook function)))
 
 ;; Currently unused.
-(defmacro add-one-shot-hook (hook function)
+(defun add-one-shot-hook (hook function)
   "Add FUNCTION to HOOK. Remove it after its first execution."
-  (let ((wrapper-function (make-symbol "add-one-shot-hook--wrapper-function")))
-    `(progn
-       (eval-when-compile (require 'cl)) ; Used: lexical-let.
-       (lexical-let ((hook* ,hook) ; Create a closure.
-                     (function* ,function))
-         (defun ,wrapper-function ()
-           "Wrapper function that will be executed only once, and then removed from the hook."
-           (unwind-protect
-               (funcall function*)
-             (remove-hook hook* ',wrapper-function)))
-         (add-hook hook* ',wrapper-function)))))
-
-;; Currently unused.
-(defun add-one-shot-hooks (hooks function)
-  "Like `add-one-shot-hook', but accepts multiple hooks (as a list of symbols)."
-  (mapc (lambda (hook)
-          (add-one-shot-hook hook function))
-        hooks))
+  (letrec ((temp-hook (lambda (&rest _)
+                        (unwind-protect
+                            (funcall function)
+                          (remove-hook hook temp-hook)))))
+    (add-hook hook temp-hook)))
 
 (provide 'conf/utils/hooks)
