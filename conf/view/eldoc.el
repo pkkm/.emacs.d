595dbd857bbe8b1dbd1f6ddc8603b6e5714a79eb
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-19 00:41:30 +0100

    view/eldoc: show more information

--- conf/view/eldoc.el
+++ conf/view/eldoc.el
@@ -3,6 +3,10 @@
 (use-package eldoc ; Included with Emacs.
   :diminish eldoc-mode
   :config
-  (setq eldoc-idle-delay 0.1))
+  (setq eldoc-idle-delay 0.1)
+  (setq eldoc-documentation-strategy 'eldoc-documentation-compose-eagerly) ; Allow documentation strings from multiple backends.
+
+  (bind-key "C-h ." #'eldoc-doc-buffer) ; Show a buffer with results from all backends.
+  (setq eldoc-echo-area-prefer-doc-buffer t)) ; Prefer the buffer for display if it's visible.
 
 (provide 'conf/view/eldoc)
