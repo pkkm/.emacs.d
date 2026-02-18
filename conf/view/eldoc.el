57543b6cde346be25060e02264a33b7da3eb0b83
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-19 00:36:07 +0100

    view/eldoc: remove tweak that's unnecessary in Emacs 30

--- conf/view/eldoc.el
+++ conf/view/eldoc.el
@@ -3,7 +3,6 @@
 (use-package eldoc ; Included with Emacs.
   :diminish eldoc-mode
   :config
-  (setq eldoc-idle-delay 0.1)
-  (add-hook 'eval-expression-minibuffer-setup-hook #'eldoc-mode)) ; Show in minibuffer when doing `eval-expression'.
+  (setq eldoc-idle-delay 0.1))
 
 (provide 'conf/view/eldoc)
