7236a753548570eea2bcd17aa4cbf889ca936e32
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-21 16:17:29 +0100

    view/long-lines: replace column-enforce-mode with fill column indicator

--- conf/view/long-lines.el
+++ conf/view/long-lines.el
@@ -3,10 +3,10 @@
 ;; Wrap on word boundaries.
 (setq-default word-wrap t)
 
-;; A mode for highlighting text that extends beyond a certain column.
-(use-package column-enforce-mode
-  :ensure t
-  :config
-  (setq column-enforce-column 80))
+(add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
+(defun my-update-fill-column-indicator-column (&rest _)
+  (set-face-foreground 'fill-column-indicator (face-background 'highlight nil t)))
+(add-hook 'enable-theme-functions #'my-update-fill-column-indicator-column)
+(my-update-fill-column-indicator-column)
 
 (provide 'conf/view/long-lines)
