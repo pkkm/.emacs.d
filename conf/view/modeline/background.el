7c35fad924509fe14337ce0dcab8ba1f74faa808
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-21 16:16:40 +0100

    view/modeline/background: use an alist instead of a hash table

--- conf/view/modeline/background.el
+++ conf/view/modeline/background.el
@@ -1,40 +1,37 @@
 ;;; Change the background of the modeline according to Evil state. -*- lexical-binding: t -*-
 
-(use-package ht :ensure t)
-
 (with-eval-after-load 'evil
-  (require 'ht)
   (require 'conf/utils/colors) ; Used: my-color-mix.
 
-  (defvar my-modeline-evil-state-to-background (ht)
-    "A hash-table maping Evil state to modeline background color, e.g. (ht ('normal \"#373b41\") ('insert \"#2e325d\"))")
-  (defvar my-modeline-other-evil-state-background (face-background 'mode-line-active nil t)
-    "Modeline background color for Evil states that aren't in `my-modeline-evil-state-to-background'.")
+  (defvar my-evil-state-modeline-colors '()
+    "An alist maping Evil state to modeline background color, e.g. '((normal \"#373b41\") (insert \"#2e325d\"))")
+  (defvar my-evil-state-modeline-default-color (face-background 'mode-line-active nil t)
+    "Modeline background color for Evil states that aren't in `my-evil-state-modeline-colors'.")
 
   ;; Calculate backgrounds for various states depending on color theme.
-  (defun my-update-mode-line-backgrounds (&rest _)
+  (defun my-update-evil-state-modeline-colors (&rest _)
     "Calculate the modeline backgrounds for various Evil states."
     (let ((original-background (face-background 'mode-line-active nil t)))
-      (setq my-modeline-evil-state-to-background
-            (ht ('normal original-background)
-                ('motion original-background)
-                ('operator original-background)
-                ('insert (my-color-mix "blue" 0.2 original-background 0.8))
-                ('replace (my-color-mix "blue" 0.2 original-background 0.8))
-                ('visual (my-color-mix "green" 0.2 original-background 0.8))))
-      (setq my-modeline-other-evil-state-background
+      (setq my-evil-state-modeline-colors
+            `((normal . ,original-background)
+              (motion . ,original-background)
+              (operator . ,original-background)
+              (insert . ,(my-color-mix "blue" 0.2 original-background 0.8))
+              (replace . ,(my-color-mix "blue" 0.2 original-background 0.8))
+              (visual . ,(my-color-mix "green" 0.2 original-background 0.8))))
+      (setq my-evil-state-modeline-default-color
             (my-color-mix "black" 0.55 original-background 0.45))))
-  (add-hook 'enable-theme-functions #'my-update-mode-line-backgrounds)
-  (my-update-mode-line-backgrounds)
+  (add-hook 'enable-theme-functions #'my-update-evil-state-modeline-colors)
+  (my-update-evil-state-modeline-colors)
 
   ;; Set the background when Evil state changes.
-  (defun my-set-mode-line-background (&rest _)
+  (defun my-set-evil-state-modeline-color (&rest _)
     "Set the modeline background according to the current Evil state."
     (set-face-background 'mode-line-active
-                         (ht-get my-modeline-evil-state-to-background
-                                 evil-state
-                                 my-modeline-other-evil-state-background)))
-  (add-hook 'post-command-hook #'my-set-mode-line-background) ; Not ideal, but usually works.
-  (add-hook 'enable-theme-functions #'my-set-mode-line-background t)) ; Append so that we run after `my-update-mode-line-backgrounds'.
+                         (alist-get evil-state
+                                    my-evil-state-modeline-colors
+                                    my-evil-state-modeline-default-color)))
+  (add-hook 'post-command-hook #'my-set-evil-state-modeline-color)
+  (add-hook 'enable-theme-functions #'my-set-evil-state-modeline-color t)) ; Append so that we run after `my-update-evil-state-modeline-colors'.
 
 (provide 'conf/view/modeline/background)
