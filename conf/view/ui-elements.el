6358bfcdccd7d0788db5cb493d46c476b7678e67
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-21 16:16:14 +0100

    view/ui-elements: modernize code

--- conf/view/ui-elements.el
+++ conf/view/ui-elements.el
@@ -1,10 +1,8 @@
-;;; UI elements (toolbar, menubar, etc). -*- lexical-binding: t -*-
+;;; Visible UI elements. -*- lexical-binding: t -*-
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-
 (scroll-bar-mode -1)
-(when (fboundp 'horizontal-scroll-bar-mode)
-  (horizontal-scroll-bar-mode -1))
+(horizontal-scroll-bar-mode -1)
 
 (provide 'conf/view/ui-elements)
