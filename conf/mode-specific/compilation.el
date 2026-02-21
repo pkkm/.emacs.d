761e4e29c25dd9c766186d914652187e8e640e8e
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-21 19:01:20 +0100

    mode-specific/compilation: add support for ANSI colors

--- conf/mode-specific/compilation.el
+++ conf/mode-specific/compilation.el
@@ -3,11 +3,14 @@
 (use-package compile ; Bundled with Emacs.
   :config
   (with-eval-after-load 'evil
-    ;; Make compilation buffers start in Emacs mode.
+    ;; Make compilation buffers start in Emacs state.
     (evil-set-initial-state 'compilation-mode 'emacs)
 
     ;; Keybindings.
     (evil-define-key 'motion compilation-mode-map "r" #'recompile)
-    (evil-define-key 'normal compilation-mode-map "r" #'recompile))) ; Necessary to shadow the usual binding.
+    (evil-define-key 'normal compilation-mode-map "r" #'recompile) ; Necessary to shadow the usual binding.
+
+    ;; Support ANSI colors.
+    (add-hook 'compilation-filter-hook #'ansi-color-compilation-filter)))
 
 (provide 'conf/mode-specific/compilation)
