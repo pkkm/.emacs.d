f9be4fcf999faa5185205967caebcc89040691c6
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-26 23:35:03 +0100

    utils/lists: prefer built-in string and list functions

--- conf/utils/lists.el
+++ conf/utils/lists.el
@@ -1,22 +1,19 @@
 ;;; Utilities for lists. -*- lexical-binding: t -*-
 
-(use-package s :ensure t :commands s-join)
 (defun join-nonempty (separator &rest parts)
   "Return a string with nonempty (not nil or \"\") elements of PARTS joined with SEPARATOR."
   (declare (indent defun))
-  (s-join separator (delq nil (delete "" parts))))
+  (string-join (delq nil (delete "" parts)) separator))
 
 (defun add-to-list-after (list-var element after-what)
   "Add ELEMENT to the value of LIST-VAR if it isn't there yet.
 ELEMENT will be added after the first occurrence of AFTER-WHAT,
 or at the beginning if AFTER-WHAT isn't in the list. Comparisons
 are done with `equal'."
-  (unless (member element (symbol-value list-var))
-    (let* ((lst (symbol-value list-var))
-           (after-position (seq-position lst after-what)))
-      (set list-var
-           (-insert-at (if after-position (1+ after-position) 0)
-                       element
-                       (symbol-value list-var))))))
+  (let ((lst (symbol-value list-var)))
+    (unless (member element lst)
+      (if-let ((tail (member after-what lst)))
+          (setcdr tail (cons element (cdr tail)))
+        (set list-var (cons element lst))))))
 
 (provide 'conf/utils/lists)
