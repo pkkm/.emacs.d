e52fc07188ef87759e66eeeb610d9dca3d509424
Paweł Kraśnicki <paul.krasnicki@gmail.com>
2026-02-28 22:01:33 +0100

    evil-specific/little-word: use built-in subword package

--- conf/evil-specific/little-word.el
+++ conf/evil-specific/little-word.el
@@ -1,16 +1,14 @@
 ;;; "Little word" -- like `w', but for CamelCase and under_scores. -*- lexical-binding: t -*-
 
 (with-eval-after-load 'evil
-  (evil-define-key 'motion 'global (kbd "C-w") #'evil-little-word)
+  (autoload 'subword-forward "subword")
 
   (evil-define-motion evil-little-word (count)
-    "Move by COUNT little words (words inside CamelCase or under_scores)."
+    "Move by COUNT little words (CamelCase, snake_case, or numbers)."
     :type exclusive
-    (let* ((case-fold-search nil)
-           (count (if count count 1)))
-      (dotimes (unused-counter count nil)
-        (forward-char)
-        (search-forward-regexp "[_A-Z]\\|\\W" nil t)
-        (backward-char)))))
+    (let ((count (or count 1)))
+      (subword-forward count)))
+
+  (evil-define-key 'motion 'global (kbd "C-w") #'evil-little-word))
 
 (provide 'conf/evil-specific/little-word)
